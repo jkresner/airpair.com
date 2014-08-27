6f634697908bdafb31c6bf5c297ce0eb53d39c95
Jonathon Kresner <jkresner@gmail.com>
2014-08-27 16:36:59 -0700

    Added less compile / watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
+path = require('path');
+
 var gulp = require('gulp');
     nodemon = require('gulp-nodemon'),
-    jshint = require('gulp-jshint');
-    // uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    less = require('gulp-less');
     // rename = require('gulp-rename'),
     // concat = require('gulp-concat'),
     // livereload = require('gulp-livereload');
+
+paths = {
+    less: 'app/styles/*.less'
+}
 
 gulp.task('lint', function () {
   gulp.src('./**/*.js')
     .pipe(jshint())
 });
 
+gulp.task('less', function () {
+  gulp.src(paths.less)
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
 
 
 gulp.task('nodemon', function () {
@@ -21,6 +34,10 @@ gulp.task('nodemon', function () {
     })
 });
 
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']);
+});
 
 
-gulp.task('default', ['nodemon']);
+gulp.task('default', ['nodemon','less','watch']);
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/hello.txt', function(req, res){
   res.send('Hello Uri Worldly');
 });
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 var server = app.listen(3333, function() {
     console.log('Listening on port %d', server.address().port);
