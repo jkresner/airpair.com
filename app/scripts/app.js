7a93236dedc64f93196f9ef6af007e77fbbb3134
Jonathon Kresner <jkresner@gmail.com>
2014-08-28 20:27:15 -0700

    First menu bar iteration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,3 +3,20 @@ angular.module("AirPair", [])
   .run(['$rootScope', function($rootScope) {
     $rootScope.title = 'Hello Airpair';
   }])
+
+	.controller('UserTagsCtrl', function($scope) {
+		$scope.tags = [];
+		$scope.add = function(tag) {
+			$scope.tags.push(tag);
+		}
+	})
+
+  .directive('tagsList', function() {
+	  return {
+		  restrict: 'E',
+		  templateUrl : '/sidenav.html',
+		  controller : ['$attrs', '$scope', function($attrs, $scope) {
+	  		$scope.mytags = $scope.$eval($attrs.tags);
+	  	}]
+	  };
+	});
