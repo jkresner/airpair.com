e0b38cf098916c0f7b275eaf413990a3a24f5d3f
Jonathon Kresner <jkresner@gmail.com>
2014-08-29 13:41:16 -0700

    New blog rendering approach
    - Add marked for markdown parsing
    - MomentJS for handling dates
    - Replace express-handlebars with express-hbs so we can use async
    helpers
    - Moved rendering setup inside of blog.js
    - Create an hbs template that wraps markdown helper to read markdown
    files

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 var express = require('express');
-var handlebars = require('express-handlebars');
 var app = express();
 var livereload = require('connect-livereload');
 
-app.engine('html', handlebars());
-app.set('views', __dirname + '/app');
-
 app.use(livereload({ port: 35729 }));
+
 
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/dist'));
 
 require('./blog')(app);
+app.set('views', __dirname + '/app');
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
