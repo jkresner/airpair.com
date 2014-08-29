1341c16eaf69a5e7b0236ccef98df1b28d251b02
Jonathon Kresner <jkresner@gmail.com>
2014-08-29 13:41:16 -0700

    New blog rendering approach
    - Add marked for markdown parsing
    - MomentJS for handling dates
    - Replace express-handlebars with express-hbs so we can use async
    helpers
    - Moved rendering setup inside of blog.js
    - Create an hbs template that wraps markdown helper to read markdown
    files

--- blog.js
+++ blog.js
@@ -1,18 +1,40 @@
-var posts = [
-	'0827:90-days-of-airpairing-on-airpair',
-	'0828:setting-up-an-angaularjs-app',
-	'0820:using-harmony-es6-with-nodejs',	
+var posts = [	
+	{ id: '0827', tag: 'mean-stack', title: 'Starting a Mean Stack App', by: 'hackerpreneur' },
+	{ id: '0828', tag: 'mean-stack', title: 'My First AngularJS App', by: 'hackerpreneur' },
+	{ id: '0829', tag: 'mean-stack', title: 'Using ES6 (Harmony) with NodeJS', by: 'hackerpreneur' }
 ];
 
+var moment = require('moment');
+var marked = require('marked');
+var fs = require('fs');
+
+var hbs = require('express-hbs');
+var hbsEngine = hbs.express3({ partialsDir: [] });
+
+hbs.registerAsyncHelper('mdEntry', function(entryId, cb) {
+  fs.readFile(__dirname + '/app/blog/'+entryId+'.md', 'utf8', function(err, md) {
+    cb(new hbs.SafeString(marked(md, { sanitize: true })));
+  });
+});
+
+hbs.registerHelper('isoMoment', function(moment) {
+  return moment.toISOString();
+});
 
 module.exports = function(app)
 {	
-	var get = function(entry, slugUrl) { 
-		app.get('/blog/mean-stack/'+slugUrl, function(req,res,next) { 
-			res.status(200).render('./blog/'+entry+'.html'); }); 
-	}
+	app.engine('hbs', hbsEngine);
  
+  var getPost = function(post, day) {
+		post.date = moment(post.id+'2014','MMDDYYYY');
+		post.published = post.date.format('DD MMMM, YYYY');	
+		post.url = '/'+post.by+'/'+post.tag+'/'+post.title.toLowerCase().replace(/ /g, '-');
+		app.get(post.url, function(req,res,next) { 
+			res.status(200).render('./blog/template.hbs', post); 
+		}); 
+  }
+
   for (var i = 0; i < posts.length; i++) {
-    get(posts[i].split(':')[0], posts[i].split(':')[1]);	
+  	getPost(posts[i], i);
   }
 };
