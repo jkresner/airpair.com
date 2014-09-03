df58d22eb9dba8fb93c8a05902194e53ea9eb4a6
Jonathon Kresner <jkresner@gmail.com>
2014-09-02 18:10:40 -0700

    New half done blog posts

--- blog.js
+++ blog.js
@@ -1,7 +1,7 @@
 var posts = [	
-	{ id: '0827', tag: 'javascript', title: 'Starting a Mean Stack App', by: 'hackerpreneur' },
+	{ id: '0901', tag: 'javascript', title: 'Using ES6 Harmony with NodeJS', by: 'hackerpreneur' },
 	{ id: '0828', tag: 'angularjs', title: 'Setting up my First AngularJS App', by: 'hackerpreneur' },
-	{ id: '0829', tag: 'mean-stack', title: 'Using ES6 (Harmony) with NodeJS', by: 'hackerpreneur' }
+	{ id: '0827', tag: 'mean-stack', title: 'Starting a Mean Stack App', by: 'hackerpreneur' }
 ];
 
 import initHBSEngine from './handlebars'
@@ -14,6 +14,7 @@ export function blogInit(app)
 		post.date = moment(post.id+'2014','MMDDYYYY');
 		post.published = post.date.format('DD MMMM, YYYY');	
 		post.url = `/posts/${post.tag}/${post.title.toLowerCase().replace(/ /g, '-')}`;
+		$log('post.url', post.url);
 		app.get(post.url, (req,res,next) => 
 			res.status(200).render('./blog/template.hbs', post)); 
 	}
