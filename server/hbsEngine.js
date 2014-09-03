812b60d579e1c84ef33bbcb60b8a851ca321eacc
Jonathon Kresner <jkresner@gmail.com>
2014-09-03 15:05:21 -0700

    Reorganize app folders for less headF*
    - ‘server’ now contains all server js and server templates
    - ‘public’ now contains all front end resources including less
    - not longer look at app (now ‘server’) for serving static files
    - killed a few no longer used files e.g. angular sideNav

--- server/hbsEngine.js
+++ server/hbsEngine.js
@@ -4,17 +4,18 @@ var hbs = require('express-hbs');
 
 export default function(app) {
 
-	var hbsEngine = hbs.express3({ partialsDir: `${app.dir}/app/partials` });
+	var hbsEngine = hbs.express3({ partialsDir: `${app.dir}/server/views/partials` });
 
 	hbs.registerAsyncHelper('mdEntry', (entryId, cb) => {
 		var file = entryId.replace('.','');
-		fs.readFile(`${app.dir}/app/blog/${file}.md`, 'utf8', (err, md) =>
+		fs.readFile(`${app.dir}/server/blog/${file}.md`, 'utf8', (err, md) =>
 			cb(new hbs.SafeString(marked(md, { sanitize: false })))
 		);
 	});
 
 	hbs.registerHelper('isoMoment', date => date.toISOString());
 
-	app.set('views', app.dir + '/app');
+	app.set('views', app.dir + '/server/views');
 	app.engine('hbs', hbsEngine);
+
 }
