54e4fb38179232a410b30e8a328d7ec635c9b634
Jonathon Kresner <jkresner@gmail.com>
2014-09-04 11:57:38 -0700

    Add crude config to globals

--- server/global.js
+++ server/global.js
@@ -1,5 +1,8 @@
+import config from './config'
+
 // A couple of handing globals (this won't get out of hand)
 
 global.$log           = console.log
 // global._              = require('lodash');
-global.moment   			= require('moment');
+global.moment   			= require('moment')
+global.config         = config
