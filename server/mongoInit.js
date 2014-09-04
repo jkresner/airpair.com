c3f93d873998eb0fe2a219117fab885df3e2fe82
Jonathon Kresner <jkresner@gmail.com>
2014-09-04 11:57:45 -0700

    Connect to mongo with mongoose

---
+++ server/mongoInit.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose')
+
+export function connect() {
+
+  mongoose.connect(config.mongoUri)
+
+  var db = mongoose.connection
+  
+  db.on('error', (e) => $log('mongo connection error:' + e) )
+  db.once('open', () => $log(`connected to db ${config.mongoUri}`) )
+
+  $log('got through mongoconnect')
+
+  return db
+}
+
+export function setSessionStore(express, app, store) {
+
+  // MongoSessionStore = require('connect-mongo')(express)
+  // storeOptions = url: "#{config.mongoUri}/sessions", auto_reconnect: true
+
+ //  app.use express.session({
+ //   cookie : { path: '/', httpOnly: true, maxAge: 2419200000 },
+ //   secret: 'airpairawesome',
+ //   store: mongoStore
+ //  })
+
+}
