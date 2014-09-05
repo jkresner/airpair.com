8e9d135d3fbe72e545d4d25e32cd60b6ed7ac9ed
Obie Fernandez <obiefernandez@gmail.com>
2014-09-05 16:48:55 -0400

    major improvements - fix rememberMe login - better layout rendering - better notifications

--- app/chat/admin.js
+++ app/chat/admin.js
@@ -22,26 +22,69 @@ angular.module("AirPair", ['firebase','angularMoment'])
     };
   }])
 
-  .controller('ChatAdminController', ['$scope', '$firebase', '$firebaseSimpleLogin', function(scope, $firebase, $firebaseSimpleLogin) {
+  .controller('ChatAdminController', ['$rootScope', '$scope', '$firebase', '$firebaseSimpleLogin', function($rootScope, $scope, $firebase, $firebaseSimpleLogin) {
+    // for debugging
+    window.scope = $scope;
 
-    scope.addMessage = function() {
+    // Create a Firebase Simple Login object
+    $scope.auth = $firebaseSimpleLogin(rootRef);
+
+    // Start with no user logged in
+    $scope.user = null;
+
+    // Upon successful login, set the user object
+    // happens automatically when rememberMe is enabled
+    $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
+      $scope.user = user;
+      console.log("Logged in as", user);
+
+      // load channels
+      $scope.channels = $firebase(channelsRef).$asArray();
+
+      // load notifications
+      $scope.notifications = $firebase(notificationsRef.child(user.uid)).$asObject();
+
+      // site presence
+      $scope.user.active = true;
+      usersRef.child($scope.user.uid).set($scope.user);
+
+      // set user to inactive on disconnect
+      usersRef.child($scope.user.uid).child('active').onDisconnect().set(false);
+
+      // load people into sidebar
+      $scope.users = $firebase(usersRef).$asArray();
+    });
+
+    // Upon successful logout, reset the user object
+    $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
+      $scope.user = null;
+    });
+
+    $scope.addMessage = function() {
       msg = {
         from: {
-          email: scope.user.email,
-          name: scope.user.displayName,
-          first_name: scope.user.thirdPartyUserData.given_name,
-          picture: scope.user.thirdPartyUserData.picture
+          email: $scope.user.email,
+          name: $scope.user.displayName,
+          first_name: $scope.user.thirdPartyUserData.given_name,
+          picture: $scope.user.thirdPartyUserData.picture
         },
-        user_id: scope.user.uid,
-        content: scope.message,
+        user_id: $scope.user.uid,
+        content: $scope.message,
         sent_at: Firebase.ServerValue.TIMESTAMP
       }
-      scope.activeMessages.$add(msg);
-      scope.message = "";
-      $('#message').focus();
+
+      // add the message
+      $scope.activeMessages.$add(msg).then(function(){
+        // make the last active channel pop to the top of the list
+        channelsRef.child($scope.activeChannel.$id).setPriority(Firebase.ServerValue.TIMESTAMP);
+        // clear the input field
+        $scope.message = "";
+        // ...and give it focus
+        $('#message').focus();
+      });
     }
 
-    scope.checkAway = function(user) {
+    $scope.checkAway = function(user) {
       if(user.last_seen) {
         var delta = (+new Date) - user.last_seen;
         console.log('checkAway', user.displayName, delta);
@@ -51,31 +94,36 @@ angular.module("AirPair", ['firebase','angularMoment'])
       }
     }
 
-    scope.clearNotifications = function(channel, user) {
+    $scope.clearNotifications = function(channel, user) {
+      if(!channel) return;
       rootRef.child("notifications").child(user.uid).child(channel.$id).remove()
     }
 
-    scope.create = function() {
-      createChannel(scope.name, scope);
-      scope.newChannelName = "";
+    $scope.create = function() {
+      createChannel($scope.name, $scope);
+      $scope.newChannelName = "";
     }
 
-    scope.lastSeen = function() {
-      if(scope.user) {
+    $scope.isLoaded = function(channel) {
+      return $scope.activeChannel && $scope.activeChannel.$id === channel.$id
+    }
+
+    $scope.lastSeen = function() {
+      if($scope.user) {
         console.log("last seen being set");
-        usersRef.child(scope.user.uid).child('last_seen').set(Firebase.ServerValue.TIMESTAMP);
+        usersRef.child($scope.user.uid).child('last_seen').set(Firebase.ServerValue.TIMESTAMP);
       }
     }
 
-    scope.load = function(slug) {
-      if(scope.user) {
+    $scope.load = function(slug) {
+      if($scope.user) {
         var activeChannelRef = channelsRef.child(slug);
-        scope.activeChannel = $firebase(activeChannelRef).$asObject();
-        scope.activeMessages = $firebase(activeChannelRef.child('messages')).$asArray();
+        $scope.activeChannel = $firebase(activeChannelRef).$asObject();
+        $scope.activeMessages = $firebase(activeChannelRef.child('messages')).$asArray();
 
         // channel participation
-        activeChannelRef.child("members").child(scope.user.uid).set(true);
-        scope.clearNotifications(scope.activeChannel, scope.user);
+        activeChannelRef.child("members").child($scope.user.uid).set(true);
+        $scope.clearNotifications($scope.activeChannel, $scope.user);
         $('#message').focus();
       }
       else {
@@ -83,41 +131,18 @@ angular.module("AirPair", ['firebase','angularMoment'])
       }
     }
 
-    scope.login = function() {
-      $firebaseSimpleLogin(rootRef).$login('google',{rememberMe: true}).then(function(user){
-        scope.user = user;
-        console.log("Logged in as", user);
-
-        // load channels
-        scope.channels = $firebase(channelsRef).$asArray();
-
-        // load notifications
-        scope.notifications = $firebase(notificationsRef.child(user.uid)).$asObject();
-
-        // site presence
-        scope.user.active = true;
-        usersRef.child(scope.user.uid).set(scope.user);
-
-        // set user to inactive on disconnect
-        usersRef.child(scope.user.uid).child('active').onDisconnect().set(false);
-
-        // load people into sidebar
-        scope.users = $firebase(usersRef).$asArray();
-      });
+    $scope.login = function() {
+      $firebaseSimpleLogin(rootRef).$login('google',{rememberMe: true});
     }
 
-    scope.talkWith = function(otherUser) {
-      var myFirstName = scope.user.thirdPartyUserData.given_name;
+    $scope.talkWith = function(otherUser) {
+      var myFirstName = $scope.user.thirdPartyUserData.given_name;
       var theirFirstName = otherUser.thirdPartyUserData.given_name;
       var name = myFirstName + " + " + theirFirstName;
 
       // todo: don't re-create if exists already, just load
-      createChannel(name, scope);
+      createChannel(name, $scope);
     }
-
-    // try triggering a login (auto-popup doesn't work on all browsers)
-    scope.login();
-
   }])
 
   .directive('scrollGlue', function(){
@@ -150,6 +175,26 @@ angular.module("AirPair", ['firebase','angularMoment'])
           }
         });
       }
+    };
+  })
+
+  .filter('reverse', function() {
+    function toArray(list) {
+      var k, out = [];
+      if( list ) {
+        if( angular.isArray(list) ) {
+          out = list;
+        }
+        else if( typeof(list) === 'object' ) {
+          for (k in list) {
+            if (list.hasOwnProperty(k)) { out.push(list[k]); }
+          }
+        }
+      }
+      return out;
+    }
+    return function(items) {
+      return toArray(items).slice().reverse();
     };
   });
 
