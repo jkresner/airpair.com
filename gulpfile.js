e5db70a754a6ba7879f9db4e837b75b2710abc58
Jonathon Kresner <jkresner@gmail.com>
2014-09-08 12:07:49 -0700

    Pre-set share widget height

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('less', function () {
 
 gulp.task('watch', function() {
   livereload.listen({ port: 35729 });
-  var watching = ['public/**','!public/style/**',paths.blog,paths.views]
+  var watching = ['./public/**','!./public/style/**',paths.blog,paths.views]
 
   gulp.watch(paths.styles, ['less']);
   gulp.watch(watching).on('change',livereload.changed);
