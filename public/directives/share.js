e4dbbe0a4433fd67170152acb530c07dd91ab6f7
Jonathon Kresner <jkresner@gmail.com>
2014-09-08 08:49:10 -0700

    Created a directive for po.st sharing widget
    - installed angular-load for async script loading
    - used angular-load promise to initialize po.st asynchronously

---
+++ public/directives/share.js
@@ -0,0 +1,21 @@
+angular.module("APShare", ['angularLoad'])
+
+  .directive('apShare', function(angularLoad) {
+    
+    var ngLoadPromise = angularLoad
+      .loadScript('//i.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6&retina=true&init=lazy');
+    
+    window.pwidget_config = { shareQuote: false, afterShare: false }; 
+
+    return {
+      templateUrl: '/directives/share.html',
+      link: function(scope, element) {
+        
+        ngLoadPromise.then(function(){
+          post_init(element[0]);
+        });
+
+      }
+    };
+
+  });
