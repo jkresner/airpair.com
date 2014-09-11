204bfe3a40af8836982ac1e97d782c628d590c3b
Jonathon Kresner <jkresner@gmail.com>
2014-09-11 09:17:14 -0700

    Add User Mongoose Model
    - Change Workshop Model pattern to be consistent

---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+var Schema, User, mongoose;
+
+mongoose = require('mongoose');
+
+Schema = mongoose.Schema;
+
+User = new Schema({
+
+  name: String,
+  email: String,
+
+  initialis: String, 
+
+  googleId: {
+    type: String,
+    index: {
+      unique: true,
+      dropDups: true
+    }
+  },
+  google: {},
+  githubId: Number,
+  github: {},
+  twitterId: Number,
+  twitter: {},
+  linkedinId: String,
+  linkedin: {},
+  stackId: Number,
+  stack: {},
+  bitbucketId: String,
+  bitbucket: {},
+  
+});
+
+// ap v.0 schema
+  // name: String,
+  // email: String,
+  // pic: String,
+  // githubId: Number,
+  // github: {},
+  // googleId: {
+  //   required: true,
+  //   type: String,
+  //   index: {
+  //     unique: true,
+  //     dropDups: true
+  //   }
+  // },
+  // google: {},
+  // twitterId: Number,
+  // twitter: {},
+  // linkedinId: String,
+  // linkedin: {},
+  // stackId: Number,
+  // stack: {},
+  // bitbucketId: String,
+  // bitbucket: {},
+  // referrer: {}
+
+export default mongoose.model('User', User);
