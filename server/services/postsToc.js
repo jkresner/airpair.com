977cd5e3e42ac6e4eca1ef56ae9c1dc8fda6b7be
Jonathon Kresner <jkresner@gmail.com>
2014-09-13 16:42:28 -0700

    Fix has links to match marked

--- server/services/postsToc.js
+++ server/services/postsToc.js
@@ -72,75 +72,26 @@ function getHtmlHeaders(lines, maxHeaderNo) {
 
 ////////////----------------------------------------------
 
-// https://github.com/joyent/node/blob/192192a09e2d2e0d6bdd0934f602d2dbbf10ed06/tools/doc/html.js#L172-L183 
-function getNodejsId(text, repetition) {
-  text = text.replace(/[^a-z0-9]+/g, '_');
-  text = text.replace(/^_+|_+$/, '');
-  text = text.replace(/^([^a-z])/, '_$1');
-
-  // If no repetition, or if the repetition is 0 then ignore. Otherwise append '_' and the number.
-  // An example may be found here: http://nodejs.org/api/domain.html#domain_example_1
-  if (repetition) {
-    text += '_' + repetition;
-  }
-
-  return text;
-}
-
-function basicGithubId(text) {
-  return text.replace(/ /g,'-')
+function getHashId(text, repetition) {
+  var id = text.replace(/ /g,'-')
+    // single chars that are removed
+    .replace(/\./g,'-')
     // escape codes
     .replace(/%([abcdef]|\d){2,2}/ig, '')
     // single chars that are removed
-    .replace(/[\/?:\[\]`.,()*"';{}+<>]/g,'')
-    ;
-          
-}
-
-function getGithubId(text, repetition) {
-  text = basicGithubId(text);
+    .replace(/[\/?:\[\]`,()*"';{}+<>]/g,'');
 
   // If no repetition, or if the repetition is 0 then ignore. Otherwise append '-' and the number.
   if (repetition) {
-    text += '-' + repetition;
+    id += '-' + repetition;
   }
 
-  return text;
+  return id;
 }
 
-/**
- * Generates an anchor for the given header and mode.
- * 
- * @name anchorMarkdownHeader
- * @function
- * @param header      {String} The header to be anchored.
- * @param mode        {String} The anchor mode ('github.com'|'nodejs.org|bitbucket.org').
- * @param repetition  {Number} The nth occurrence of this header text, starting with 0. Not required for the 0th instance.
- * @param moduleName  {String} The name of the module of the given header (required only for 'nodejs.org' mode).
- * @return            {String} The header anchor that is compatible with the given mode.
- */
-function anchor(header, mode, repetition, moduleName) {
-  mode = mode || 'github.com';
-  var replace;
 
-  switch(mode) {
-    case 'github.com':
-      replace = getGithubId;
-      break;
-    case 'bitbucket.org':
-      replace = getBitbucketId;
-      break;
-    case 'nodejs.org':
-      if (!moduleName) throw new Error('Need module name to generate proper anchor for ' + mode);
-      replace = function (hd, repetition) {
-          return getNodejsId(moduleName + '.' + hd, repetition);
-      };
-      break;
-    default:
-      throw new Error('Unknown mode: ' + mode);
-  }
-
-  var href = replace(header.trim().toLowerCase(), repetition);
+function anchor(header, repetition) {
+  var href = getHashId(header.trim().toLowerCase(), repetition);
 
   return '[' + header + '](#' + href + ')';
 };
@@ -151,8 +102,8 @@ function anchor(header, mode, repetition, moduleName) {
 
 function notNull(x) { return  x !== null; }
 
-function addAnchor(mode, header) {
-  header.anchor = anchor(header.name, mode, header.instance);
+function addAnchor(header) {
+  header.anchor = anchor(header.name, header.instance);
   return header;
 }
 
@@ -233,8 +184,7 @@ function countHeaders (headers) {
 }
 
 export default function(content, maxHeaderNo) {
-  var mode = 'github.com';
-  maxHeaderNo = maxHeaderNo || 4;
+  var maxHeaderNo = maxHeaderNo || 4;
 
   // $log('transform.content', content)
 
@@ -250,7 +200,7 @@ export default function(content, maxHeaderNo) {
 
   var allHeaders    =  countHeaders(headers)
     , lowestRank    =  _(allHeaders).chain().pluck('rank').min().value()
-    , linkedHeaders =  _(allHeaders).map(addAnchor.bind(null, mode));
+    , linkedHeaders =  _(allHeaders).map(addAnchor.bind(null));
 
   if (linkedHeaders.length === 0) return { transformed: false };
 
