7cf271ab3139d9520f1ad351d0c320946db7d643
Jonathon Kresner <jkresner@gmail.com>
2014-09-16 18:30:30 -0700

    Show db backed posts & display post with meta info

--- server/hbsEngine.js
+++ server/hbsEngine.js
@@ -6,15 +6,16 @@ export default function(app) {
 
 	var hbsEngine = hbs.express3({ partialsDir: `${app.dir}/server/views/partials` });
 
-	hbs.registerAsyncHelper('mdEntry', (entryId, cb) => {
-		var file = entryId.replace('.','');
-		fs.readFile(`${app.dir}/server/blog/${file}.md`, 'utf8', (err, md) =>
-			cb(new hbs.SafeString(marked(md, { sanitize: false })))
-		);
+	hbs.registerAsyncHelper('mdEntry', (md, cb) => {
+		cb(new hbs.SafeString(marked(md, { sanitize: false })))
 	});
 
-	hbs.registerHelper('isoMoment', date => date.toISOString());
-	hbs.registerHelper('dateFormat', (date, format) => date.format(format));
+	hbs.registerAsyncHelper('mdToc', (md, cb) => {
+		cb(new hbs.SafeString(marked(md, { sanitize: false })))
+	});
+
+	hbs.registerHelper('isoMoment', date => moment(date).toISOString());
+	hbs.registerHelper('dateFormat', (date, format) => moment(date).format(format));
 
 	app.set('views', app.dir + '/server/views');
 	app.engine('hbs', hbsEngine);
