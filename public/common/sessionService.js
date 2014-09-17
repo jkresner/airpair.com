2491c580d5284a02c774a28c5fff970631998394
Jonathon Kresner <jkresner@gmail.com>
2014-09-16 22:36:49 -0700

    ** Impl smart client-side authorization routing
    - Use factory cache to re-hit cache for data in each controller
    - Create onAuthenticated and onUnauthenticated promises in
    SessionService
    - User resolver in routes to stop controller execution

--- public/common/sessionService.js
+++ public/common/sessionService.js
@@ -2,13 +2,31 @@ angular.module('APSvcSession', [])
 
   .constant('API', '/v1/api')
 
-  .service('SessionService', ['$http', 'API', function($http, API) {
-    this.getSession = function(success, error)
-    {
-      $http.get(`${API}/session`).success(success).error(error);
+  .service('SessionService', ['$http', 'API', '$cacheFactory', function($http, API, $cacheFactory) {
+    var cache;
+    this.getSession = function() {
+      cache = cache || $cacheFactory();
+      return $http.get(`${API}/session/full`, { cache : cache }).then(
+          function(response) { return response.data; }
+        );
     }
+
+    this.onAuthenticated = function(fn) {
+      return this.getSession().then(fn);
+    }
+
+    this.onUnauthenticated = function(fn) {
+      return this.getSession().then(null, fn);
+    }
+
+    this.flushCache = function() {
+      cache = null; 
+    };
+
+    /*
     this.getSessionFull = function(success, error)
     {
       $http.get(`${API}/session/full`).success(success).error(error);
     }
+    */
   }])
