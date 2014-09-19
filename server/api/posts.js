a8270587b3f5b6b8b1307b9bf213dacb6742ca3f
Jonathon Kresner <jkresner@gmail.com>
2014-09-19 06:59:53 -0700

    v1 api refactor

--- server/api/posts.js
+++ server/api/posts.js
@@ -1,57 +1,32 @@
 import {serve, initAPI} from './_api'
 import * as Svc from '../services/posts'
 import {authd} from '../identity/auth/middleware'
-
-var API = initAPI(Svc)
-
-function me(req, cb) {
-  Svc.getUsersPosts.call(this, req.user._id, cb)
-}
-
-function toc(req, cb) {
-  Svc.getTableOfContents.call(this, req.body.md, cb)
-}
-
-function create(req, cb) {
-  Svc.create.call(this, req.body, cb)
-}
-
-function update(req, cb) {
-  Svc.update.call(this, req.params.id, req.body, cb)
-}
-
-function publish(req, cb) {
-  Svc.publish.call(this, req.params.id, req.body, cb)
-}
-
-function recent(req, cb) {
-  Svc.getRecentPublished.call(this, cb)
-}
-
-function byuser(req, cb) {
-  Svc.getUsersPublished.call(this, req.params.id, cb)
-}
-
-function deleteById(req, cb) {
-  Svc.deleteById.call(this, req.params.id, cb)
-}
-
 var auth = authd({isApiRequest:true})
+
+var actions = {
+  me: function(req, cb) { Svc.getUsersPosts.call(this, req.user._id, cb) },
+  toc: function(req, cb) { Svc.getTableOfContents.call(this, req.body.md, cb) },
+  publish: function(req, cb) { Svc.publish.call(this, req.params.id, req.body, cb) },
+  recent: function(req, cb) { Svc.getRecentPublished.call(this, cb) },
+  byuser: function(req, cb) { Svc.getUsersPublished.call(this, req.params.id, cb) },
+}
+
+var API = initAPI(Svc, actions)
 
 export default class {
 
   constructor(app) {
-    app.get('/posts/recent', serve(recent))
-    app.get('/posts/me', auth, serve(me))     
+    app.get('/posts/recent', API.recent)
+    app.get('/posts/me', auth, API.me)     
     app.get('/posts/:id', API.detail)  
-    app.get('/posts/by/:id', serve(byuser))     
+    app.get('/posts/by/:id', API.byuser)     
 
-    app.post('/posts', auth, serve(create))     
-    app.post('/posts-toc', auth, serve(toc))     
-    app.put('/posts/:id', auth, serve(update))  
-    app.put('/posts/publish/:id', auth, serve(publish))      
+    app.post('/posts', auth, API.create)     
+    app.post('/posts-toc', auth, API.toc)     
+    app.put('/posts/:id', auth, API.update)  
+    app.put('/posts/publish/:id', auth, API.publish)      
 
-    app.delete('/posts/:id', auth, serve(deleteById))      
+    app.delete('/posts/:id', auth, API.delete)      
   }
 
 }
