87ffe897c0cfa4dad3af99c3289dccbd06c6b018
Jonathon Kresner <jkresner@gmail.com>
2014-09-20 14:45:08 -0700

    Move http helpers to be shared across specs

--- test/server/bootstrap.js
+++ test/server/bootstrap.js
@@ -1,18 +1,17 @@
-var System = require('es6-module-loader').System
-var initConfig = require('./../../server/config')
-var setGlobals = require('./../../server/global')
-
-var sessionSpec = require('./sessionSpec')
 global.users    = require('./../data/users')
 global.sinon    = require('sinon')
 global.chai     = require('chai')
 global.expect   = chai.expect
-
-var app = null
+var System = require('es6-module-loader').System
+var initConfig = require('./../../server/config')
+var setGlobals = require('./../../server/global')
 
 
 var config = initConfig('test', __dirname)
 setGlobals(config)
+
+var sessionSpec = require('./sessionSpec')
+var authSpec = require('./authSpec')
 
 
 describe('Server Tests', function() {
@@ -29,4 +28,26 @@ describe('Server Tests', function() {
 })
 
   
+global.http = require('supertest')
+global.logging = false
+global.cookie = null //-- used for maintaining login
 
+global.login = function(initials, cb) {
+  if (logging) $log('login:', '/test/setlogin/'+initials)
+  return http(global.app).get('/test/setlogin/'+initials).end(function(e,res){
+    cookie = res.headers['set-cookie']
+    cb()
+  })
+}
+
+global.get = function(url) {
+  var apiUrl = '/v1/api'+url
+  if (logging) $log('get:', apiUrl)
+  return http(global.app).get(apiUrl).expect('Content-Type', /json/)
+}
+
+global.post = function(url, data) {
+  var apiUrl = '/v1/api'+url
+  if (logging) $log('post:', apiUrl)
+  return http(global.app).post(apiUrl).send(data).expect('Content-Type', /json/)
+}
--- test/server/sessionSpec.js
+++ test/server/sessionSpec.js
@@ -1,52 +1,35 @@
-var http = require('supertest')
-var logging = false
-var cookie; 
-var get = function(url) {
-  var apiUrl = '/v1/api'+url
-  if (logging) $log('get:', apiUrl)
-  return http(global.app).get(apiUrl).expect('Content-Type', /json/);
-}
-
-var login = function(initials, cb) {
-  if (logging) $log('login:', '/test/setlogin/'+initials)
-  return http(global.app).get('/test/setlogin/'+initials).end(function(e,res){
-    cookie = res.headers['set-cookie'];
-    cb()
-  })
-}
-
 module.exports = function()
 {
   describe("API", function() {
 
     it('gets 403 on unauthenticated session', function(done) {
       get('/session').expect(403)
-        .end(function(err, res){
-          if (err) throw err;
+        .end(function(e, r){
+          if (e) throw e;
           done()
-        });
+        })
     })
   
     it('gets 403 on unauthenticated full session', function(done) {
       get('/session/full').expect(403)
-        .end(function(err, res){
-          if (err) throw err;
+        .end(function(e, r){
+          if (e) throw e;
           done()
-        });
+        })
     })
 
     it('gets slim authenticated session', function(done) {
       login('scap', function() {
         get('/session').set('cookie',cookie).expect(200)
-          .end(function(err, res){
-            var s = res.body
+          .end(function(e, r){
+            var s = r.body
             expect(s._id).to.equal("5418c03f8f8c80299bcc4783")
             expect(s.email).to.equal("sc@airpair.com")
             expect(s.name).to.equal("Shane")
             expect(s.avatar).to.equal("//0.gravatar.com/avatar/54856fdf0610d64c79bf82b43d56f356")
             done()
-          });    
-      });
+          })    
+      })
     })
   }) 
 }
