afa9f8ded2cdcfaddcef1ecbed27d32c8c19331a
Jonathon Kresner <jkresner@gmail.com>
2014-09-22 18:09:47 -0700

    Some nice syntax sugar on routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,28 +8,32 @@ export default function(app) {
   
   var router = require('express').Router()
 
+    .get('/session', authd, UsersAPI.getSessionLite)
+    .get('/session/full', authd, UsersAPI.getSessionByUserId)   
 
-  router.get('/session', authd, UsersAPI.getSessionLite)
-  router.get('/session/full', authd, UsersAPI.getSessionByUserId)   
-  router.put('/adm/users/role/:id/:role', adm, UsersAPI.toggleUserInRole)   
+    .get('/tags/search/:id', TagsAPI.search)    
+    .get('/tags/:slug', TagsAPI.getBySlug)
+    
+    .get('/posts/recent', PostsAPI.getRecentPublished)
+    .get('/posts/me', authd, PostsAPI.getUsersPosts)     
+    .get('/posts/:id', PostsAPI.getById)  
+    .get('/posts/by/:id', PostsAPI.getUsersPublished)     
+    .post('/posts', authd, PostsAPI.create)     
+    .post('/posts-toc', authd, PostsAPI.getTableOfContents)     
+    .put('/posts/:id', authd, PostsAPI.update)  
+    .put('/posts/publish/:id', authd, PostsAPI.publish)   
+    .delete('/posts/:id', authd, PostsAPI.deleteById)
+    
 
-  router.get('/tags/search/:id', TagsAPI.search)    
-  router.get('/tags/:slug', TagsAPI.getBySlug)
+    .get('/workshops/', WorkshopsAPI.getAll)
+    .get('/workshops/:id', WorkshopsAPI.getBySlug)
 
-  router.get('/posts/recent', PostsAPI.getRecentPublished)
-  router.get('/posts/me', PostsAPI.getUsersPosts)     
-  router.get('/posts/:id', PostsAPI.getById)  
-  router.get('/posts/by/:id', PostsAPI.getUsersPublished)     
-  router.post('/posts', authd, PostsAPI.create)     
-  router.post('/posts-toc', authd, PostsAPI.getTableOfContents)     
-  router.put('/posts/:id', authd, PostsAPI.update)  
-  router.put('/posts/publish/:id', authd, PostsAPI.publish)   
-  router.delete('/posts/:id', authd, PostsAPI.deleteById)
-  
-  router.get('/workshops/', WorkshopsAPI.getAll)
-  router.get('/workshops/:id', WorkshopsAPI.getBySlug)
+  var admrouter = require('express').Router()
+    .use(adm)
+    .put('/users/role/:id/:role', UsersAPI.toggleUserInRole)   
 
-  
+  router.use('/adm',admrouter)
+
   return router
 
 }
