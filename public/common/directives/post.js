84c020c38ed20cf9cd21fc0c70814a0ccf42e6b9
Jonathon Kresner <jkresner@gmail.com>
2014-09-23 09:10:49 -0700

    Fix item directive updating bug

--- public/common/directives/post.js
+++ public/common/directives/post.js
@@ -6,19 +6,20 @@ angular.module("APPost", [])
     return {
       restrict: 'E',
       template: require('./postListItem.html'),
-      scope: {
-        p: '=post'
-      },
+      // scope: {
+      //   p: '=post'
+      // },
       link: function(scope, element, attrs) {
-        scope.url = scope.p.url;
-        scope.title = scope.p.title;
-        scope.published = scope.p.published;
-        scope.publishedFormat = moment(scope.p.published).format('d0 MMM, YYYY');
-        scope.by = { 
-          name: scope.p.by.name,                        
-          avatar: scope.p.by.avatar };                        
-        scope.meta = { description: scope.p.meta.description };                      
-        scope.tags = scope.p.tags;  
+        scope.post = scope.$eval(attrs.post)
+        // scope.url = scope.p.url;
+        // scope.title = scope.p.title;
+        // scope.published = scope.p.published;
+        // scope.publishedFormat = moment(scope.p.published).format('d0 MMM, YYYY');
+        // scope.by = { 
+        //   name: scope.p.by.name,                        
+        //   avatar: scope.p.by.avatar };                        
+        // scope.meta = { description: scope.p.meta.description };                      
+        // scope.tags = scope.p.tags;  
       }
     };
 
