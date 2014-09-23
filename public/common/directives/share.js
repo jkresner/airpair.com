52ae3cb2e2f7fbaf0b78001455a270291b70d2bd
Jonathon Kresner <jkresner@gmail.com>
2014-09-23 12:09:50 -0700

    Fix po.st https loading bug

--- public/common/directives/share.js
+++ public/common/directives/share.js
@@ -2,8 +2,11 @@ angular.module("APShare", ['angularLoad'])
 
   .directive('apShare', function(angularLoad) {
     
+    var src = ('https:' == document.location.protocol ? 'https://s' : 'http://i')
+      + '.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6&retina=true&init=lazy';
+
     var ngLoadPromise = angularLoad
-      .loadScript('//i.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6&retina=true&init=lazy');
+      .loadScript(src);
     
     window.pwidget_config = { shareQuote: false, afterShare: false }; 
 
