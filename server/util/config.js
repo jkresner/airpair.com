aba4f0386ad8075798f036ddb62840fa8f91c6fc
Jonathon Kresner <jkresner@gmail.com>
2014-09-24 19:20:22 -0700

    Update config build version

--- server/util/config.js
+++ server/util/config.js
@@ -1,5 +1,5 @@
 var cfg = {
-  build:  { version: '1.03', deployed: 'Sep 19' },
+  build:  { version: '1.04', deployed: 'Sep 25' },
   port:     process.env.PORT || 3333,
   mongoUri: process.env.MONGOHQ_URL || "mongodb://localhost/airpair_dev",
   session: { secret: 'airyv1' },
