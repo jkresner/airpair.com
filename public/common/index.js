1f2b998a49f4c251ad3a459f1060d6042db1ffa1
Jonathon Kresner <jkresner@gmail.com>
2014-10-16 14:51:12 -0400

    Very basic side nav
    - Toggle nag backed by local storage

--- public/common/index.js
+++ public/common/index.js
@@ -1,6 +1,7 @@
 require('./../common/directives/share.js');
 require('./../common/directives/post.js');
 require('./../common/directives/tagInput.js');
+require('./../common/directives/sideNav.js');
 require('./../common/directives/analytics.js');
 require('./../common/filters/filters.js');
 require('./../common/models/postsService.js');
@@ -10,28 +11,31 @@ require('./../auth/module.js');
 require('./../posts/module.js');
 require('./../workshops/module.js');
 
-angular.module("AP", ['ngRoute', 'APAuth', 'APPosts', 'APWorkshops'])
+angular.module("AP", ['ngRoute', 'APSideNav', 'APAuth', 'APPosts', 'APWorkshops'])
 
-  .config(['$locationProvider', '$routeProvider', 
+  .config(['$locationProvider', '$routeProvider',
       function($locationProvider, $routeProvider) {
-  
+
     $locationProvider.html5Mode(true);
 
   }])
 
-  .run(['$rootScope', '$location', 'SessionService', 
+  .run(['$rootScope', '$location', 'SessionService',
     function($rootScope, $location, SessionService) {
 
-    
+
     $rootScope.$on('$routeChangeSuccess', function() {
       window.trackRoute($location.path());
+
+      // Hack to hide menu on login pages
+      $('nav#side').toggle(!($location.path().indexOf('/auth') != -1))
     });
 
 
     // SessionService.onAuthenticated( (session) => {
     //   $rootScope.session = session;
     // })
-  
+
   }])
 
 ;
