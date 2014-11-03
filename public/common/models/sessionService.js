f2cb6f7eb59dd9240f5598b651338fd26d81ebe5
Michael Perrenoud <mperren@gmail.com>
2014-11-03 14:27:45 -0700

    Merge branch 'manage-tags-bookmarks' into tags-and-bookmarks
    Conflicts:
    gulpfile.js
    public/common/directives/sideNav.html
    public/common/directives/sideNav.js
    public/common/directives/stack.html
    public/common/directives/tagInput.html
    public/styles/components.less
    server/api/users.js
    server/routes/api.js
    shared/util.js

--- public/common/models/sessionService.js
+++ public/common/models/sessionService.js
@@ -60,6 +60,10 @@ angular.module('APSvcSession', [])
       $http.put(`${API}/users/me/tag/${data.slug}`, {}).success(setScope(success)).error(error)
     }
 
+    this.sortTags = function(data, success, error) {
+      $http.put(`${API}/users/me/sortTags`, data).success(success).error(error);
+    }
+
     this.updateBookmark = function(data, success, error)
     {
       $http.put(`${API}/users/me/bookmarks/${data.type}/${data.objectId}`, {}).success(setScope(success)).error(error)
