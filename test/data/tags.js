ace7b4b772888eeabcd3367ff36090fb9b36a0ce
Michael Perrenoud <mperren@gmail.com>
2014-11-10 20:39:24 -0700

    Adding tests for the tag search modifications.

--- test/data/tags.js
+++ test/data/tags.js
@@ -28,6 +28,22 @@ module.exports = {
     "soId" : "mongodb",
     "slug" : "mongodb"
   },
+  mongoengine: {
+    "_id" : "5369a5b21c67d1a4859d311a",
+    "desc" : "MongoEngine is a Document-Object Mapper (think ORM, but for document databases) for working with MongoDB from Python. It uses a simple declarative API, similar to the Django ORM.",
+    "name" : "mongoengine",
+    "short" : "mongoengine",
+    "soId" : "mongoengine",
+    "slug" : "mongoengine"
+  },
+  mongoid: {
+    "_id" : "5181d0aa66a6f999a465ee0c",
+    "desc" : "Mongoid is an Object-Document-Mapper (ODM) for MongoDB written in Ruby.",
+    "name" : "mongoid",
+    "short" : "mongoid",
+    "soId" : "mongoid",
+    "slug" : "mongoid"
+  },
   mean: {
     "_id" : "53c54f5e8f8c80299bcc39bf",
     "desc" : "The MEAN stack is a methodology that employs MongoDB, Express, AngularJS and Node.js to provide a complete end-to-end system. One of the primary motivations of such a stack is that it is pure JavaScript at all ends.\r\n\r\nTwo different implementations are available: http://mean.io and http://meanjs.org/",
@@ -35,8 +51,8 @@ module.exports = {
     "short" : "mean-stack",
     "soId" : "mean-stack",
     "slug" : "mean-stack"
-	},
-	rails: {
+  },
+  rails: {
     "_id" : "514825fa2a26ea020000002f",
     "__v" : 0,
     "desc" : "Ruby on Rails is an open source full-stack web application framework written in Ruby. It follows the popular MVC framework model and is known for its &quot;convention over configuration&quot; approach to application development.",
@@ -45,5 +61,37 @@ module.exports = {
     "soId" : "ruby-on-rails",
     "tokens" : "",
     "slug" : "ruby-on-rails"
-	}
+  },
+  ios: {
+    "_id" : "514825fa2a26ea020000001b", 
+    "desc" : "iOS is Apple&#39;s operating system for mobile devices, such as the iPhone, iPod touch, iPad and Apple TV (2nd generation and up). Much is shared with OS X, but iOS is optimized for touch-based interfaces.",
+    "name" : "ios",
+    "short" : "ios",
+    "slug" : "ios",
+    "soId" : "ios",
+  },
+  ios4: {
+    "_id" : "5181d0a966a6f999a465ecbd",
+    "desc" : "The 2010-2011 version of the iPhone/iPad/iPod Touch/Apple TV operating system family, made by Apple.",
+    "name" : "ios4",
+    "short" : "ios4",
+    "soId" : "ios4",
+    "slug" : "ios4"
+  },
+  ios5: {
+    "_id" : "5181d0a966a6f999a465eb8c",
+    "desc" : "The version of iOS (formerly iPhone OS) was released by Apple on 13 Oct 2011. iOS 5 runs on iPhone 3GS, iPhone 4, iPhone 4S, iPad and iPod Touch devices 3rd and 4th generation.",
+    "name" : "ios5",
+    "short" : "ios5",
+    "soId" : "ios5",
+    "slug" : "ios5"
+  },
+  iosSimulator: {
+    "_id" : "5181d0aa66a6f999a465edfd",
+    "desc" : "The iOS Simulator application presents the iPhone or iPad user interface in a window on the computer to emulate iPhone or iPad devices. This application provides several ways of interacting with it by using the keyboard and mouse to simulate taps, device rotation, and other user actions.",
+    "name" : "ios-simulator",
+    "short" : "ios-simulator",
+    "soId" : "ios-simulator",
+    "slug" : "ios-simulator"
+  }
 }
