77cd2928f81844f96c175948a59fa66b7165fb91
Steve Purves <stevejpurves@gmail.com>
2014-11-14 11:09:14 +0000

    reverted

--- public/v1/js/adm.js
+++ public/v1/js/adm.js
@@ -20,10 +20,10 @@ angular.module("ADM", ['ngRoute', 'APSvcSession', 'ADMPosts', 'ADMUsers', 'ADMRe
 
 
 },{"./../adm/posts/module.js":4,"./../adm/redirects/module.js":6,"./../adm/users/module.js":8,"./../common/directives/post.js":10,"./../common/directives/tagInput.js":13,"./../common/filters/filters.js":14,"./../common/models/adminDataService.js":15,"./../common/models/postsService.js":16,"./../common/models/sessionService.js":17}],2:[function(require,module,exports){
-module.exports = "<article itemscope=\"itemscope\" itemtype=\"http://schema.org/BlogPosting\" itemprop=\"blogPost\">\r\n <header class=\"entry-header\">\r\n    <img class=\"entry-header-image\" itemprop=\"image\" ng-src=\"{{ post.meta.ogImage }}\" align=\"left\" />\r\n\r\n    <span class=\"entry-categories\">\r\n      <a ng-repeat='tag in post.tags' href=\"#\" title=\"View all posts in {{ tag.name }}\" rel=\"category tag\">{{ '{'+tag.slug+'}' }}</a>\r\n    </span>\r\n\r\n    <p class=\"entry-meta\">\r\n      <span class=\"entry-author\" itemprop=\"author\" itemscope=\"itemscope\" itemtype=\"http://schema.org/Person\">\r\n        <span class=\"entry-author-name\" itemprop=\"name\">{{ post.by.name }}</span>\r\n      </span>\r\n    <img class=\"entry-author-image\" itemprop=\"image\" ng-src=\"{{ post.by.avatar }}?s=50\" align=\"left\" />\r\n      <br />Updated\r\n      {{ post.updated | agoTime }}\r\n      <span ng-if=\"post.published\">,\r\n      Published {{ post.published | agoTime }}</span>\r\n      <time class=\"entry-time\" itemprop=\"datePublished\" datetime=\"{{ post.published }}\">{{ post.publishedFormat }}</time>\r\n    </p>\r\n  </header>\r\n  <div class=\"entry-content\" itemprop=\"text\">\r\n    <h2 class=\"entry-title\" itemprop=\"headline\">{{ post.title }}</h2>\r\n    <p>{{ post.meta.description }}</p>\r\n  </div>\r\n\r\n  <footer class=\"entry-footer\" style=\"width:50%\">\r\n    <ul>\r\n      <li><a href=\"/posts/edit/{{ post._id }}\" target=\"_blank\">Edit</a></li>\r\n      <li><a href=\"/posts/publish/{{ post._id }}\" target=\"_blank\">Publish</a></li>\r\n      <li ng-if=\"post.published\"><a href=\"{{ post.url }}\" target=\"_blank\">View</a></li>\r\n    </ul>\r\n\r\n    <p class=\"entry-meta\">\r\n\r\n    </p>\r\n  </footer>\r\n</article>\r\n";
+module.exports = "<article itemscope=\"itemscope\" itemtype=\"http://schema.org/BlogPosting\" itemprop=\"blogPost\">\n <header class=\"entry-header\">\n    <h2 class=\"entry-title\" itemprop=\"headline\">{{ post.title }}</h2>\n    <p class=\"entry-meta\">\n      <time class=\"entry-time\" itemprop=\"datePublished\" datetime=\"{{ post.published }}\">{{ post.publishedFormat }}</time>\n      <span class=\"entry-author\" itemprop=\"author\" itemscope=\"itemscope\" itemtype=\"http://schema.org/Person\">\n        <span class=\"entry-author-name\" itemprop=\"name\">{{ post.by.name }}</span>\n      </span>\n      <br />Updated\n      {{ post.updated | agoTime }}\n      <span ng-if=\"post.published\">,\n      Published {{ post.published | agoTime }}</span>\n    </p>\n  </header>\n  <div class=\"entry-content\" itemprop=\"text\">\n    <img class=\"entry-author-image\" itemprop=\"image\" ng-src=\"{{ post.by.avatar }}?s=50\" align=\"left\" />\n    <p>{{ post.meta.description }}</p>\n  </div>\n\n  <footer class=\"entry-footer\">\n    <ul>\n      <li><a href=\"/posts/edit/{{ post._id }}\" target=\"_blank\">Edit</a></li>\n      <li><a href=\"/posts/publish/{{ post._id }}\" target=\"_blank\">Publish</a></li>\n      <li ng-if=\"post.published\"><a href=\"{{ post.url }}\" target=\"_blank\">View</a></li>\n    </ul>\n\n    <p class=\"entry-meta\">\n      <span class=\"entry-categories\">\n        <a ng-repeat='tag in post.tags' href=\"#\" title=\"View all posts in {{ tag.name }}\" rel=\"category tag\">{{ '{'+tag.slug+'}' }}</a>\n      </span>\n    </p>\n  </footer>\n</article>\n";
 
 },{}],3:[function(require,module,exports){
-module.exports = "<section id=\"posts\">\r\n\r\n  <h3>Recently updated <span><a href=\"/posts/all\" target=\"_blank\">All posts</a></span></h3>\r\n  <div class=\"posts recent\">\r\n    <ap-post-list-item post=\"p\" ng-repeat=\"p in recent\"></ap-post-list-item>\r\n  </div>\r\n\r\n</section>\r\n";
+module.exports = "<section id=\"posts\">\n\n  <h3>Recently updated <span><a href=\"/posts/all\" target=\"_blank\">All posts</a></span></h3>\n  <div class=\"posts recent\">\n    <ap-post-list-item post=\"p\" ng-repeat=\"p in recent\"></ap-post-list-item>\n  </div>\n\n</section>\n";
 
 },{}],4:[function(require,module,exports){
 "use strict";
@@ -49,7 +49,7 @@ angular.module("ADMPosts", ['ngRoute', 'APSvcAdmin', 'APSvcPosts', 'APFilters'])
 
 
 },{"./item.html":2,"./list.html":3}],5:[function(require,module,exports){
-module.exports = "<section id=\"redirects\">\r\n\r\n  <h3>Redirects </h3>\r\n\r\n  <div style=\"float:right\">note a restart is necessary for the redirects to take effect</div>\r\n\r\n  <input type=\"text\" ng-model='previous' placeholder=\"From\" />\r\n  <input type=\"text\" ng-model='current' placeholder=\"To\" />\r\n  <button id=\"btnCreateRedirect\" ng-click=\"createRedirect()\" class=\"btn\">Create</button>\r\n\r\n  <hr />\r\n\r\n  <div class=\"redirects\">\r\n    <ul>\r\n      <li ng-repeat=\"r in redirects\">\r\n        <time>{{ r._id | objectIdToDate : 'MM.DD' }}</time>\r\n        <label><a href=\"{{ r.previous }}\" target=\"_blank\">{{ r.previous }}</a></label>\r\n        <span><a href=\"#\" ng-click=\"deleteRedirect(r._id)\">x</a>===> {{ r.current }}</span>\r\n      </li>\r\n    </ul>\r\n  </div>\r\n\r\n</section>\r\n";
+module.exports = "<section id=\"redirects\">\n\n  <h3>Redirects </h3>\n\n  <div style=\"float:right\">note a restart is necessary for the redirects to take effect</div>\n\n  <input type=\"text\" ng-model='previous' placeholder=\"From\" />\n  <input type=\"text\" ng-model='current' placeholder=\"To\" />\n  <button id=\"btnCreateRedirect\" ng-click=\"createRedirect()\" class=\"btn\">Create</button>\n\n  <hr />\n\n  <div class=\"redirects\">\n    <ul>\n      <li ng-repeat=\"r in redirects\">\n        <time>{{ r._id | objectIdToDate : 'MM.DD' }}</time>\n        <label><a href=\"{{ r.previous }}\" target=\"_blank\">{{ r.previous }}</a></label>\n        <span><a href=\"#\" ng-click=\"deleteRedirect(r._id)\">x</a>===> {{ r.current }}</span>\n      </li>\n    </ul>\n  </div>\n\n</section>\n";
 
 },{}],6:[function(require,module,exports){
 "use strict";
@@ -85,7 +85,7 @@ angular.module("ADMRedirects", ['ngRoute', 'APSvcAdmin', 'APFilters']).config(['
 
 
 },{"./list.html":5}],7:[function(require,module,exports){
-module.exports = "<section id=\"users\">\r\n\r\n  <br /><br />\r\n  <input type=\"text\" ng-model=\"_id\" placeholder=\"Type userId\" style=\"width:300px\" />\r\n  <select ng-model=\"role\">\r\n    <option>admin</option>\r\n    <option>editor</option>\r\n  </select>\r\n  <button class=\"btn\" ng-click=\"toggleRole()\">Toggle role</button>\r\n\r\n\r\n  <h3>Admins</h3>\r\n  <ul class=\"users admin\">\r\n    <li ng-repeat=\"user in admins\">{{ user.name }}</li>\r\n  </ul>\r\n\r\n  <h3>Editors</h3>\r\n  <ul class=\"users editors\">\r\n    <li ng-repeat=\"user in editors\">{{ user.name }}</li>\r\n  </ul>\r\n\r\n</section>\r\n";
+module.exports = "<section id=\"users\">\n\n  <br /><br />\n  <input type=\"text\" ng-model=\"_id\" placeholder=\"Type userId\" style=\"width:300px\" />\n  <select ng-model=\"role\">\n    <option>admin</option>\n    <option>editor</option>\n  </select>\n  <button class=\"btn\" ng-click=\"toggleRole()\">Toggle role</button>\n\n\n  <h3>Admins</h3>\n  <ul class=\"users admin\">\n    <li ng-repeat=\"user in admins\">{{ user.name }}</li>\n  </ul>\n\n  <h3>Editors</h3>\n  <ul class=\"users editors\">\n    <li ng-repeat=\"user in editors\">{{ user.name }}</li>\n  </ul>\n\n</section>\n";
 
 },{}],8:[function(require,module,exports){
 "use strict";
@@ -118,7 +118,7 @@ angular.module("ADMUsers", ['ngRoute', 'APSvcAdmin', 'APFilters']).config(['$loc
 
 
 },{"./list.html":7}],9:[function(require,module,exports){
-module.exports = "<div class=\"preview\">\r\n  <article class=\"blogpost\">\r\n    <h1 class=\"entry-title\" itemprop=\"headline\">{{ post.title || \"Type post title ...\" }}</h1>\r\n    <h4 id=\"table-of-contents\" ng-if=\"preview.toc\">Table of Contents</h4>\r\n    <ul id=\"previewToc\" ng-bind-html=\"preview.toc | markdownHtml\"></ul>\r\n    <figure class=\"author\">\r\n      <img ng-alt=\"{{post.by.name}}\" ng-src=\"{{post.by.avatar}}?s=100\">\r\n      <figcaption>\r\n        {{post.by.bio}}\r\n      </figcaption>\r\n    </figure>\r\n    <p class=\"asset\" ng-bind-html=\"preview.asset | markdownHtml\" ng-if=\"post.title && post.by.bio\"></p>\r\n    <hr />\r\n    <div id=\"body\" ng-bind-html=\"preview.body | markdownHtml\"></div>\r\n  </article>\r\n</div>\r\n\r\n<hr />\r\n";
+module.exports = "<div class=\"preview\">\n  <article class=\"blogpost\">\n    <h1 class=\"entry-title\" itemprop=\"headline\">{{ post.title || \"Type post title ...\" }}</h1>\n    <h4 id=\"table-of-contents\" ng-if=\"preview.toc\">Table of Contents</h4>\n    <ul id=\"previewToc\" ng-bind-html=\"preview.toc | markdownHtml\"></ul>\n    <figure class=\"author\">\n      <img ng-alt=\"{{post.by.name}}\" ng-src=\"{{post.by.avatar}}?s=100\">\n      <figcaption>\n        {{post.by.bio}}\n      </figcaption>\n    </figure>\n    <p class=\"asset\" ng-bind-html=\"preview.asset | markdownHtml\" ng-if=\"post.title && post.by.bio\"></p>\n    <hr />\n    <div id=\"body\" ng-bind-html=\"preview.body | markdownHtml\"></div>\n  </article>\n</div>\n\n<hr />\n";
 
 },{}],10:[function(require,module,exports){
 "use strict";
@@ -146,10 +146,10 @@ angular.module("APPost", []).directive('apPostListItem', ['$parse', function($pa
 
 
 },{"./post.html":9,"./postListItem.html":11}],11:[function(require,module,exports){
-module.exports = "<article itemscope=\"itemscope\" itemtype=\"http://schema.org/BlogPosting\" itemprop=\"blogPost\">\r\n  <a href=\"{{ post.url }}\" title=\"{{ post.title }}\" target=\"_self\" rel=\"bookmark\">\r\n    <header class=\"entry-header\">\r\n      <img class=\"entry-header-image\" itemprop=\"image\" ng-src=\"{{ post.meta.ogImage }}\" align=\"left\" />\r\n\r\n      <span class=\"entry-categories\">\r\n        <em ng-repeat='tag in post.tags'>{{ '{'+tag.slug+'}' }}</em>\r\n      </span>\r\n\r\n      <p class=\"entry-meta\">\r\n\r\n        <span class=\"entry-author\" itemprop=\"author\" itemscope=\"itemscope\" itemtype=\"http://schema.org/Person\">\r\n          <span class=\"entry-author-name\" itemprop=\"name\">{{ post.by.name }}</span>\r\n          <img class=\"entry-author-image\" itemprop=\"image\" ng-src=\"{{ post.by.avatar }}?s=50\" align=\"left\" />\r\n          <time class=\"entry-time\" itemprop=\"datePublished\" datetime=\"{{ post.published }}\">{{ post.publishedFormat }}</time>\r\n\r\n        </span>\r\n      </p>\r\n    </header>\r\n    <div class=\"entry-content\" itemprop=\"text\">\r\n      <h2 class=\"entry-title\" itemprop=\"headline\">{{ post.title }}</h2>\r\n      <p>{{ post.meta.description }}</p>\r\n\r\n    </div>\r\n  </a>\r\n  <footer class=\"entry-footer\">\r\n    <bookmarker type=\"post\" object-id=\"post._id\"></bookmarker>\r\n  </footer>\r\n</article>\r\n";
+module.exports = "<article itemscope=\"itemscope\" itemtype=\"http://schema.org/BlogPosting\" itemprop=\"blogPost\">\n  <a href=\"{{ post.url }}\" title=\"{{ post.title }}\" target=\"_self\" rel=\"bookmark\">\n    <header class=\"entry-header\">\n      <h2 class=\"entry-title\" itemprop=\"headline\">{{ post.title }}</h2>\n      <p class=\"entry-meta\">\n        <time class=\"entry-time\" itemprop=\"datePublished\" datetime=\"{{ post.published }}\">{{ post.publishedFormat }}</time>\n        by\n        <span class=\"entry-author\" itemprop=\"author\" itemscope=\"itemscope\" itemtype=\"http://schema.org/Person\">\n          <span class=\"entry-author-name\" itemprop=\"name\">{{ post.by.name }}</span>\n        </span>\n      </p>\n    </header>\n    <div class=\"entry-content\" itemprop=\"text\">\n      <img class=\"entry-author-image\" itemprop=\"image\" ng-src=\"{{ post.by.avatar }}?s=50\" align=\"left\" />\n      <p>{{ post.meta.description }}</p>\n    </div>\n  </a>\n  <footer class=\"entry-footer\">\n    <p class=\"entry-meta\">\n      <span class=\"entry-categories\">\n        <a ng-repeat='tag in post.tags' ng-href=\"/posts/tag/{{tag.slug}}\" title=\"View all posts in {{ tag.name }}\" rel=\"category tag\">{{ '{'+tag.slug+'}' }}</a>\n      </span>\n    </p>\n\n    <bookmarker type=\"post\"></bookmarker>\n  </footer>\n</article>\n";
 
 },{}],12:[function(require,module,exports){
-module.exports = "<div class=\"form-group tag-input-group\">\r\n  <div class=\"nomobile\">This feature is not available on mobile</div>\r\n\r\n  <div class=\"drag\">\r\n    <p>\r\n      <b>drag in order of importance</b>\r\n      <br />place most used first\r\n    </p>\r\n  </div>\r\n  <div class=\"selected\">\r\n    <label for=\"tagInput\">Your stack</label>\r\n\r\n    <ul class=\"tags\" sortable get='tags' set='updateTags'>\r\n      <li ng-repeat=\"tag in tags() | orderBy:'sort'\" ng-attr-data-id=\"{{tag._id}}\">\r\n        {{tag.slug}}\r\n        <a class=\"remove\" ng-click=\"deselectMatch(tag)\">x</a>\r\n        <a class=\"order\" href=\"#\"></a>\r\n      </li>\r\n    </ul>\r\n\r\n    <p ng-if=\"!tags() || tags().length == 0\">No tags selected yet.</p>\r\n  </div>\r\n\r\n  <label for=\"tagInput\">Search technologies</label>\r\n  <input type=\"text\" class=\"tagInput form-control\"\r\n    placeholder=\"type a technology (e.g. javascript)\"\r\n    ng-model=\"q\"\r\n    typeahead=\"t as t for t in getTags($viewValue) | filter:$viewValue\"\r\n    typeahead-editable=\"false\"\r\n    typeahead-input-formatter=\"keypressSelect($model)\" tabindex=\"100\"\r\n    typeahead-template-url=\"tagMatch.html\"\r\n    >\r\n  <!-- typeahead-loading=\"loading\"\r\n  <i ng-show=\"loading\" class=\"glyphicon glyphicon-refresh\"></i>\r\n   -->\r\n</div>\r\n\r\n<script type=\"text/ng-template\" id=\"tagMatch.html\">\r\n  <div>\r\n    <a class=\"tagSelect\">\r\n      <span bind-html-unsafe=\"match.model.slug | typeaheadHighlight:query\"></span>\r\n      <p bind-html-unsafe=\"match.model.desc | typeaheadHighlight:query\"></p>\r\n    </a>\r\n  </div>\r\n</script>\r\n";
+module.exports = "<div class=\"form-group tag-input-group\">\n  <div class=\"nomobile\">This feature is not available on mobile</div>\n\n  <div class=\"drag\">\n    <p>\n      <b>drag in order of importance</b>\n      <br />place most used first\n    </p>\n  </div>\n  <div class=\"selected\">\n    <label for=\"tagInput\">Your stack</label>\n\n    <ul class=\"tags\" sortable sort='tags' service='tags'>\n      <li ng-repeat=\"tag in session.tags | orderBy:'sort'\" ng-attr-data-id=\"{{tag._id}}\">\n        {{tag.slug}}\n        <a class=\"remove\" ng-click=\"deselectMatch(tag)\">x</a>\n        <a class=\"order\" href=\"#\"></a>\n      </li>\n    </ul>\n\n    <p ng-if=\"!selectedTags() || selectedTags().length == 0\">No tags selected yet.</p>\n  </div>\n\n  <label for=\"tagInput\">Search technologies</label>\n  <input type=\"text\" class=\"tagInput form-control\"\n    placeholder=\"type a technology (e.g. javascript)\"\n    ng-model=\"q\"\n    typeahead=\"t as t for t in getTags($viewValue) | filter:$viewValue\"\n    typeahead-editable=\"false\"\n    typeahead-input-formatter=\"keypressSelect($model)\" tabindex=\"100\">\n  <!-- typeahead-loading=\"loading\"\n  <i ng-show=\"loading\" class=\"glyphicon glyphicon-refresh\"></i>\n   -->\n</div>\n\n<script type=\"text/ng-template\" id=\"template/typeahead/typeahead-match.html\">\n  <div>\n    <a class=\"tagSelect\">\n      <span bind-html-unsafe=\"match.model.slug | typeaheadHighlight:query\"></span>\n      <p bind-html-unsafe=\"match.model.desc | typeaheadHighlight:query\"></p>\n    </a>\n  </div>\n</script>\n";
 
 },{}],13:[function(require,module,exports){
 "use strict";
@@ -163,20 +163,11 @@ angular.module('APTagInput', ['ui.bootstrap']).value('badTagsSearchQuery', funct
   return {
     restrict: 'EA',
     template: require('./tagInput.html'),
-    scope: {},
     controller: ['$scope', '$attrs', '$http', function($scope, $attrs, $http) {
-      $scope.tags = $scope.$parent.tags;
-      $scope.selectTag = $scope.$parent.selectTag;
-      $scope.deselectTag = $scope.$parent.deselectTag;
-      $scope.updateTags = $scope.$parent.updateTags;
-      $scope.sortSuccess = $scope.$parent.sortSuccess;
-      $scope.sortFail = $scope.$parent.sortFail;
-      $scope.templateUrl = "tagMatch.html";
       $scope.getTags = function(q) {
         if (badTagsSearchQuery(q)) {
           return [];
         }
-        q = encodeURIComponent(q);
         return $http.get('/v1/api/tags/search/' + q).then(function(res) {
           var tags = [];
           angular.forEach(res.data, function(item) {
@@ -197,7 +188,7 @@ angular.module('APTagInput', ['ui.bootstrap']).value('badTagsSearchQuery', funct
         $scope.q = "";
       };
       $scope.deselectMatch = function(match) {
-        $scope.deselectTag(match);
+        this.deselectTag(match);
       };
     }]
   };
@@ -506,8 +497,6 @@ module.exports = {
       return nestedPick(obj, _.keys(selectList));
   }),
   isBot: (function(useragent) {
-    if (!useragent)
-      return false;
     var source = useragent.replace(/^\s*/, '').replace(/\s*$/, '');
     return botPattern.test(source);
   })
