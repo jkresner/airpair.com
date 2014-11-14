c7118f5b21751069316430bcab3117155b1d116d
Jonathon Kresner <jkresner@gmail.com>
2014-11-13 16:37:09 -0800

    Simplify index script mechanic
    - Use config to determine index script
    - Grab dist version from dist/v1/js

--- index.js
+++ index.js
@@ -10,35 +10,36 @@ require('./server/util/cache')
 
 export function run()
 {
-	var express = require('express')
-	var app = express()
+  var express = require('express')
+  var app = express()
 
-	//-- We don't want to serve sessions for static resources
-	//-- Save database write on every resources
-	app.use(express.static(config.appdir+'/public'))
+  //-- We don't want to serve sessions for static resources
+  //-- Save database write on every resources
+  app.use(express.static(config.appdir+'/dist'))
+  app.use(express.static(config.appdir+'/public'))
 
-	mongo.connect()
-	session(app, mongo.initSessionStore)
+  mongo.connect()
+  session(app, mongo.initSessionStore)
 
-	//-- Do not move connect-livereload before session middleware
-	if (config.livereload) app.use(require('connect-livereload')({ port: 35729 }))
+  //-- Do not move connect-livereload before session middleware
+  if (config.livereload) app.use(require('connect-livereload')({ port: 35729 }))
 
-	hbsEngine(app)
-	routes(app, () => {
-		app.use( (err, req, res, next) => {
-			$error(err, req.user, req)
-			res.status(400).send(err.message)
-		})
+  hbsEngine(app)
+  routes(app, () => {
+    app.use( (err, req, res, next) => {
+      $error(err, req.user, req)
+      res.status(400).send(err.message)
+    })
 
-		process.on('uncaughtException', (err) => {
-			$error(err, {name:'uncaught',_id:'',email:''}, null)
-			process.exit(1)
-		})
+    process.on('uncaughtException', (err) => {
+      $error(err, {name:'uncaught',_id:'',email:''}, null)
+      process.exit(1)
+    })
 
-		var server = app.listen(config.port, function() {
-		  console.log(`Listening on port ${server.address().port}`.white)
-		})
+    var server = app.listen(config.port, function() {
+      console.log(`Listening on port ${server.address().port}`.white)
+  })
 
-	})
-	return app;
+  })
+  return app;
 }
