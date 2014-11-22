5dfe480710655fedbbe7507dd83909d23977a06a
Jonathon Kresner <jkresner@gmail.com>
2014-11-22 14:25:00 -0800

    Wired up adding braintree paymethod

--- public/common/directives/payment.js
+++ public/common/directives/payment.js
@@ -9,19 +9,16 @@ angular.module("APPaymentDirectives", ['angularLoad','APSvcSession'])
     return {
       template: require('./paymentInfo.html'),
       link(scope, element) {
-        ngLoadPromise.then(function(){
 
-        });
       },
       controller($scope) {
+        ngLoadPromise.then(function(){
 
-        $scope.$watch('btoken', function(clientToken) {
-          if (!clientToken) return
-          if (braintree) {
+          $scope.$watch('btoken', function(clientToken) {
+            if (!clientToken || !braintree) return
+
             $scope.client = new braintree.api.Client({clientToken});
-            // client.tokenizeCard({number: "4111111111111111", expirationDate: "10/20"}, function (err, nonce) {
-            //   // Send nonce to your server
-            // });
+
             console.log('setup', $scope.client)
             $scope.card = {
               cardholderName: $scope.session.name,
@@ -31,35 +28,40 @@ angular.module("APPaymentDirectives", ['angularLoad','APSvcSession'])
               cardNickName: "Personal united visa"
             }
 
-            // braintree.setup(token, "custom", {id: "cardinfoForm"});
+          })
+
+          $scope.submit = (formValid, data) => {
+            console.log('formValid', formValid, data)
+            if (formValid)
+            {
+              console.log('isValid', $scope.card)
+              $scope.client.tokenizeCard(_.pick($scope.card,'cardholderName','number','expirationMonth','expirationYear'), function (err, nonce) {
+                console.log('isValid', err, nonce)
+                if (nonce)
+                {
+                  var pm = { type: 'braintree', token: nonce, name: $scope.card.cardNickName, makeDefault: true }
+
+                  var err = (e) => console.log(e)
+                  var suc = (r) => {
+                    if (!$scope.paymethods) $scope.paymethods = [r]
+                    else $scope.paymethods.push(r)
+                  }
+
+                  SessionService.addPaymethod(pm, suc, err)
+                }
+                // Send nonce to your server
+              });
+
+              // SessionService.signup(data,
+              //   (result) => {
+              //   //$modalInstance.close();
+              // },
+              //   (e) => $scope.signupFail = e.error
+              // )
+            }
           }
 
-        })
-
-        $scope.submit = (formValid, data) => {
-          console.log('formValid', formValid, data)
-          if (formValid)
-          {
-            console.log('isValid')
-            $scope.client.tokenizeCard($scope.card, function (err, nonce) {
-              console.log('isValid', err, nonce)
-              var pm = { type: 'braintree', token: nonce, name: $scope.card.cardNickName, makeDefault: true }
-
-              var err = (e) => console.log(e)
-              var suc = (r) => console.log('suc', r)
-
-              SessionService.addPaymethod(pm, suc, err)
-              // Send nonce to your server
-            });
-
-            // SessionService.signup(data,
-            //   (result) => {
-            //   //$modalInstance.close();
-            // },
-            //   (e) => $scope.signupFail = e.error
-            // )
-          }
-        }
+        });
 
       }
     };
