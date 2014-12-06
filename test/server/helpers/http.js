c2778da3a33542e91ebee9651e494e00d27cf339
Jonathon Kresner <jkresner@gmail.com>
2014-12-05 22:32:43 -0800

    Rss implementation

--- test/server/helpers/http.js
+++ test/server/helpers/http.js
@@ -5,6 +5,11 @@ global.cookieCreatedAt = null
 
 var uaFirefox = 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'
 
+global.GETXML = function(url) {
+  return GETP(url)
+    .expect('Content-Type', /application\/rss\+xml/)
+}
+
 global.GETP = function(url) {
   return http(global.app)
     .get(url)
