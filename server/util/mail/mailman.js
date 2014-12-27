df763b7645c1ebb9d79f2ddb002046b383e5415d
Jonathon Kresner <jkresner@gmail.com>
2014-12-27 12:00:22 -0800

    Send email to customer on expert available

--- server/util/mail/mailman.js
+++ server/util/mail/mailman.js
@@ -93,7 +93,8 @@ module.exports = function(mailProvider)
     },
     sendPipelinerNotifyReplyEmail(byName, expertStatus, requestId, requestByName, cb) {
       mailProvider.send(receivers.pipeliners, renderEmail('pipelinernotifyreply',
-        { byName, requestId, expertStatus, requestByName }), cb)
+        { byName, requestId, requestByName, isAvailable: expertStatus == 'available',
+          expertStatus: expertStatus.toUpperCase() }), cb)
     },
     sendExpertAvailable(toCustomer, expertName, requestId, noPaymethods, cb) {
       mailProvider.send(`${toCustomer.name} <${toCustomer.email}>`, renderEmail('expertavailable', {
