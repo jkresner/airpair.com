ae2cddbd994b18f567db78cc6063dd16d45ffa10
Jonathon Kresner <jkresner@gmail.com>
2014-12-27 12:02:23 -0800

    Fix get help Post CTA to send to v1 request

--- ang/common/pageHelpers.js
+++ ang/common/pageHelpers.js
@@ -1,5 +1,3 @@
-window.pageHlpr = {}
-
 window.storage = function(k, v) {
   if (window.localStorage)
   {
@@ -40,128 +38,135 @@ var getHighlightConfig = function(elm) {
   return cfg;
 }
 
+angular.module("APPageHelpers", [])
 
-window.pageHlpr.highlightSyntax = function(opts) {
-  var elements = document.querySelectorAll('pre > code')
-  // console.log('Prism.highlightElement', elements);
-  for (var i=0, element; element = elements[i++];) {
-    var config = getHighlightConfig(element.parentNode);
-    if (!config || !config.lang) { return }
+.factory('PageHlpr', function NotificationsFactory($rootScope) {
 
-    if (config.linenums)
-    {
-      element.parentNode.className = 'line-numbers'
-    }
-    // console.log('Prism.highlightElement', element, config);
+  this.highlightSyntax = function(opts) {
+    var elements = document.querySelectorAll('pre > code')
+    // console.log('Prism.highlightElement', elements);
+    for (var i=0, element; element = elements[i++];) {
+      var config = getHighlightConfig(element.parentNode);
+      if (!config || !config.lang) { return }
 
-    element.className = 'language-'+config.lang
-    Prism.highlightElement(element, false, function() {
-      if (opts && opts.addCtrs)
+      if (config.linenums)
       {
-        element.outerHTML+='<footer>Get expert <a class="trackPostCTA" href="/find-an-expert?utm_source=airpair.com&utm_campaign=posts-{{campPeriod}}&utm_content={{viewData.slug}}&utm_medium=website&utm_term='+config.lang+'">'+config.lang+' help</a></footer>';
+        element.parentNode.className = 'line-numbers'
       }
-    });
-  }
-}
+      // console.log('Prism.highlightElement', element, config);
 
-
-window.pageHlpr.loadDisqus = function(canonical)
-{
-  if (window.disqus_initialized) return
-  // console.log('loadDisqus')
-  window.disqus_initialized = true
-	window.disqus_url = canonical;
-	var dsq = document.createElement('script'); dsq.type = 'text/javascript';
-  dsq.async = true;
-	dsq.src = 'https://airpairblog.disqus.com/embed.js';
-	var x = document.getElementsByTagName('script')[0];
-	x.parentNode.insertBefore(dsq, x);
-}
-
-
-window.pageHlpr.loadPoSt = function()
-{
-  window.pwidget_config = { shareQuote: false, afterShare: false };
-  var p = document.createElement('script');
-  p.type = 'text/javascript';
-  p.async = true;
-  p.src = ('https:' == document.location.protocol ? 'https://s' : 'http://i')
-    + '.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6&retina=true';
-
-  var x = document.getElementsByTagName('script')[0];
-  x.parentNode.insertBefore(p, x);
-}
-
-
-window.pageHlpr.fixNavs = function(elmId)
-{
-  var scrollingOn = $(document).width() > 900;
-  var offset = 40; //$('#side').offset().top;
-
-  var fix = function(e){
-    if (window.scrollY < offset) {
-      $(elmId).css('top', offset - window.scrollY);
-    } else if (scrollingOn) {
-      $(elmId).css('top', 0);
+      element.className = 'language-'+config.lang
+      Prism.highlightElement(element, false, function() {
+        if (opts && opts.addCtrs)
+        {
+          if ($rootScope.session._id)
+            element.outerHTML+='<footer>Get expert <a class="trackPostCTA" href="/meet-experts">'+config.lang+' help</a></footer>';
+          else
+            element.outerHTML+='<footer>Like learning from posts like this? <a onclick="$(\'#postSubscribeEmail\').focus()"><b>Subscribe for more!</b></a></footer>';
+        }
+      });
     }
-  };
+  }
 
-  $(window).scroll(fix);
-  fix();
-}
-
-
-var fixRailElements = function(e)
-{
-  if ($('.railMarker').length == 0) return
-
-  var scrollingOn = $(document).width() > 900;
-  var offset = $('.railMarker').offset().top;
-  var offsetLeft = $('.railMarker').offset().left;
-  var shareHeight = $('.share').height() + 20;
-  var cta1Height = $('.rail1CTA').height() + 20;
-
-  $('.share').css('left', offsetLeft);
-  $('.rail1CTA').css('left', offsetLeft);
-
-  // console.log('scrollingOnr', offset)
-  if (scrollingOn)
+  this.loadDisqus = function(canonical)
   {
-    if (window.scrollY < offset)
+    if (window.disqus_initialized) return
+    // console.log('loadDisqus')
+    window.disqus_initialized = true
+    window.disqus_url = canonical;
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = 'https://airpairblog.disqus.com/embed.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(dsq, x);
+  }
+
+  this.loadPoSt = function()
+  {
+    window.pwidget_config = { shareQuote: false, afterShare: false };
+    var p = document.createElement('script');
+    p.type = 'text/javascript';
+    p.async = true;
+    p.src = ('https:' == document.location.protocol ? 'https://s' : 'http://i')
+      + '.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6&retina=true';
+
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(p, x);
+  }
+
+  this.fixNavs = function(elmId)
+  {
+    var scrollingOn = $(document).width() > 900;
+    var offset = 40; //$('#side').offset().top;
+
+    var fix = function(e){
+      if (window.scrollY < offset) {
+        $(elmId).css('top', offset - window.scrollY);
+      } else if (scrollingOn) {
+        $(elmId).css('top', 0);
+      }
+    };
+
+    $(window).scroll(fix);
+    fix();
+  }
+
+  var fixRailElements = function(e)
+  {
+    if ($('.railMarker').length == 0) return
+
+    var scrollingOn = $(document).width() > 900;
+    var offset = $('.railMarker').offset().top;
+    var offsetLeft = $('.railMarker').offset().left;
+    var shareHeight = $('.share').height() + 20;
+    var cta1Height = $('.rail1CTA').height() + 20;
+
+    $('.share').css('left', offsetLeft);
+    $('.rail1CTA').css('left', offsetLeft);
+
+    // console.log('scrollingOnr', offset)
+    if (scrollingOn)
     {
-      $('.share').css('top', offset - window.scrollY);
-      $('.rail1CTA').css('top', offset - window.scrollY + shareHeight);
-      $('#table-of-contents').css('top', shareHeight+cta1Height);
-      $('#table-of-contents + ul').css('top', shareHeight+cta1Height);
+      if (window.scrollY < offset)
+      {
+        $('.share').css('top', offset - window.scrollY);
+        $('.rail1CTA').css('top', offset - window.scrollY + shareHeight);
+        $('#table-of-contents').css('top', shareHeight+cta1Height);
+        $('#table-of-contents + ul').css('top', shareHeight+cta1Height);
+      }
+      else {
+        var scrollSet = window.scrollY - offset
+        $('.share').css('top', 0);
+        $('.rail1CTA').css('top', shareHeight);
+        $('#table-of-contents').css('top', scrollSet + shareHeight + cta1Height);
+        $('#table-of-contents + ul').css('top', scrollSet + shareHeight + cta1Height);
+      }
     }
-    else {
-      var scrollSet = window.scrollY - offset
-      $('.share').css('top', 0);
-      $('.rail1CTA').css('top', shareHeight);
-      $('#table-of-contents').css('top', scrollSet + shareHeight + cta1Height);
-      $('#table-of-contents + ul').css('top', scrollSet + shareHeight + cta1Height);
+    else
+    {
+      $('.railBookmarkCTA').hide()
+      $('.share').hide()
+      // if (window.scrollY < (offset + 100)) {
+        // $('.rail1CTA').css('top', offset+10 - window.scrollY);
+        // $('.rail1CTA').toggle(true)
+      // }
+      // else
+        // $('.rail1CTA').toggle(false)
     }
   }
-  else
-  {
-    $('.railBookmarkCTA').hide()
-    $('.share').hide()
-    // if (window.scrollY < (offset + 100)) {
-      // $('.rail1CTA').css('top', offset+10 - window.scrollY);
-      // $('.rail1CTA').toggle(true)
-    // }
-    // else
-      // $('.rail1CTA').toggle(false)
-  }
-}
 
-window.pageHlpr.fixPostRail = function()
-{
-  if ($('.railMarker').length > 0) {
-    $(window).scroll(fixRailElements);
-    fixRailElements();
-    setTimeout(fixRailElements, 500)
-    setTimeout(fixRailElements, 1000)
-    setTimeout(fixRailElements, 3000)
+  this.fixRailElements = fixRailElements
+
+  this.fixPostRail = function()
+  {
+    if ($('.railMarker').length > 0) {
+      $(window).scroll(fixRailElements);
+      fixRailElements();
+      setTimeout(fixRailElements, 500)
+      setTimeout(fixRailElements, 1000)
+      setTimeout(fixRailElements, 3000)
+    }
   }
-}
+
+  return this;
+})
