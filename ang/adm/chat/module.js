04775aaa75a84dff41747e27503261d617cd3fac
Abe Haskins <abeisgreat@abeisgreat.com>
2015-01-28 20:35:07 +0000

    Airchat UI and status fixes

--- ang/adm/chat/module.js
+++ ang/adm/chat/module.js
@@ -4,6 +4,29 @@ require('../../../public/lib/angular-moment/angular-moment.js');
 require('../../chat/core.js');
 require('../../chat/app.js');
 
+moment.lang('en', {
+    relativeTime : {
+        future: "in %s",
+        past:   "%s",
+        s:  "%ds",
+        m:  "1m",
+        mm: "%dm",
+        h:  "1h",
+        hh: "%dh",
+        d:  "1d",
+        dd: "%dd",
+        M:  "30d",
+        MM: function (number) {
+          return number * 30 + 'd';
+        },
+        y:  "365d",
+        yy: function (number) {
+          return number * 365 + 'd';
+        }
+    }
+});
+
+
 angular.module("ADMChat", ["chat-widget", "angularMoment"])
 
 .config(function($locationProvider, $routeProvider) {
