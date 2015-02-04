9cb6440e7ced9ded227e9352eb3a4d4220be64a5
Mike Risse <rissem@gmail.com>
2015-02-04 15:44:58 -0800

    remove '.only'

--- test/server/postsSpec.js
+++ test/server/postsSpec.js
@@ -6,7 +6,7 @@ for (var i = 0; i < 501; i++){
   lotsOfWords += "stuff ";
 }
 
-module.exports = () => describe.only("API: ", function() {
+module.exports = () => describe("API: ", function() {
 
   before(function(done) {
     SETUP.analytics.stub()
