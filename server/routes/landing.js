184e15cfcc891ae845baf046ce0b60fb3bdc9ac4
Jonathon Kresner <jkresner@gmail.com>
2015-03-03 09:16:48 -0800

    Improved landing page

--- server/routes/landing.js
+++ server/routes/landing.js
@@ -41,11 +41,11 @@ export default function(app) {
           slug: '100k-writing-competition',
           launched: 'Mon Feb 2 2015 11:00:00 GMT-0800 (PST)',
           meta:
-           { title: 'AirPair.com $100,000.00 Developer Writing Competition',
+           { title: 'Fork Up! AirPair\'s $100,000.00 Git-Powered Developer Writing Competition',
              description: 'AirPair has released a set of new Github API powered publishing tools. To celebrate, AirPair is distributing over $100k in cash prizes to the best posts submitted before May 30th, 2015.',
              canonical: 'https://www.airpair.com/100k-writing-competition',
              ogType: 'Article',
-             ogTitle: 'AirPair.com $100,000.00 Developer Writing Competition',
+             ogTitle: 'Fork Up! AirPair\'s $100,000.00 Git-Powered Developer Writing Competition',
              ogDescription: 'AirPair has released a set of new Github API powered publishing tools. To celebrate, AirPair is distributing over $100k in cash prizes to the best posts submitted before May 30th, 2015.',
              ogImage: 'https://www.airpair.com/static/img/pages/postscomp/og.png',
            },
