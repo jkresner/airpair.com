61e690b1abae13daf255c7193dc61e6dafa8e81a
Jonathon Kresner <jkresner@gmail.com>
2015-03-22 20:48:50 -0700

    Mega gulp => node => other package upgrade /clean+improve
    - Upgrade to node 0.12
    - Upgrade connect-mongo and use new initialization syntax
    - Move .less files and watch only js and css built directories
    - Remove raygun / firebase

--- build/nodemon.js
+++ build/nodemon.js
@@ -1,9 +1,12 @@
 var nodemon = require('gulp-nodemon');
 
 module.exports = () =>
-  nodemon({ script: 'bootstrap.js', ext: 'html js',
-      ignore: ['public/*','ang/*','test/*','dist/*','node_modules/*'] })
-    .on('change', ['lint'])
+  nodemon({
+      script: 'bootstrap.js',
+      ext: 'hbs js',
+      ignore: ['public/*','ang/*','test/*','dist/*','node_modules/*'],
+      tasks: ['lint']
+    })
     .on('restart', function () {
-      //console.log('>> node restart');
+      console.log('>> node restart');
     })
