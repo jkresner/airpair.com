9a1e8d80222154632664860f48753f1ce37536dc
Jonathon Kresner <jkresner@gmail.com>
2015-03-29 01:32:33 -0700

    F-ing Retarted Waste of a day

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,98 @@
-var gulp = require('gulp');
+var path      = require('path')
+var gulp      = require('gulp')
+gulp.util     = require('gulp-util')
+var red = gulp.util.colors.red
+var white = gulp.util.colors.white
+gulp.printErr = function(plugin, cb) {
+  return function(e) {
+    gulp.util.beep()
+    gulp.util.log(white(plugin), red(e.message))
+    if (cb) cb()
+  }
+}
 
-require('./build/traceur');
+var timestart     = new Date().getTime()
+var timeLast      = new Date().getTime()
+global.$time      = function(msg) {
+  var subLapsed   = (new Date().getTime() - timeLast).toString()
+  timeLast        = new Date().getTime()
+  var lapsed      = (timeLast-timestart).toString()
+  console.log(
+      (lapsed+"      a".substring(0,6-lapsed.length)).magenta,
+      (subLapsed+"        b".substring(0,8-subLapsed.length)).green,
+      msg.magenta
+    )
+}
+
+var publicDir = path.resolve('public')
+
+var cfg = {
+  watch: {
+    livereload: {
+      port: 35729,
+      all: [
+        'server/views/**', //views
+        'public/static/js/*.js',//devJS:
+        'public/static/styles/adm.css',
+        'public/static/styles/index.css'
+      ]
+    },
+    less:       'public/styles/**/*.less',
+  },
+  path: {
+    less:         'public/styles/*.less',
+    lessSrc:      {
+      index:      'public/styles/index.less',
+      adm:        'public/styles/adm.less',
+      libs:       'public/styles/libs.less',
+      all:        'public/styles/*.less'
+    },
+    builtCss:     './public/static/styles',
+    builtJS:      './public/static/js',
+
+    browserifyJS: './ang/*.js',
+    publicDir:    publicDir,
+    lessDir:      publicDir+'/styles/',
+  },
+  styleBundles:     ['libs.less', 'index.less', 'adm.less'],
+  jsBundles:        {
+    all:            ['home.js', 'index.js', 'adm.js'],
+    adm:            ['adm.js'],
+    index:          ['index.js','home.js'],
+  }
+}
+
+function initTasks(initTasks, options) {
+  initTasks.forEach(function(task) {
+    gulp.task(task, function(cb) {
+      require('./build/'+task)(gulp, cfg, options, cb)() })
+  })
+  return initTasks
+}
 
 
-['lint','less','watch','nodemon','nodemontest','clean','watchify','dist', 'devsetup']
-  .forEach(function(task) {
-    gulp.task(task, require('./build/'+task));
-})
+function initRunTasks(runTasks, options) {
+  return function(callback) {
 
+    runTasks.forEach(function(task) {
+      gulp.task(task, function(cb) {
+        require('./build/'+task)(gulp, cfg, options, cb)() })
+    })
 
-gulp.task('default', ['nodemon','less','watch','watchify']);
+    return gulp.start(runTasks)
+  }
+}
 
-gulp.task('test', ['nodemontest','build']);
+gulp.task('less:all', initRunTasks(['less'], {section:'all'}) )
+gulp.task('less:libs', initRunTasks(['less'], {section:'libs'}) )
+gulp.task('less:adm', initRunTasks(['less'], {section:'adm'}) )
+gulp.task('less:index', initRunTasks(['less'], {section:'index'}) )
 
-gulp.task('build', ['clean'], function() {
-  gulp.start(['less', 'dist']);
-});
+gulp.task('build', initTasks(['clean']),initRunTasks(['dist']))
+
+gulp.task('default', initTasks(['nodemon','less']), initRunTasks(['watch'], {section:'all'}) )
+gulp.task('index', initTasks(['nodemon','less'], {section:'index'}), initRunTasks(['watch'], {section:'index'}) )
+gulp.task('adm', initTasks(['nodemon','less'], {section:'adm'}), initRunTasks(['watch'], {section:'adm'}) )
+
+// gulp.task('test', initTasks(['less'],['nodemontest']))
+
