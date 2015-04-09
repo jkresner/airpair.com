bb79825ee33987f2fabf17ed832a20bffcd0018a
Jonathon Kresner <jkresner@gmail.com>
2015-04-09 11:40:04 +1000

    Fix gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var colors    = require('colors')
 var path      = require('path')
 var gulp      = require('gulp')
 gulp.util     = require('gulp-util')
@@ -93,7 +94,7 @@ gulp.task('less:libs', initRunTasks(['less'], null, {section:'libs'}) )
 gulp.task('less:adm', initRunTasks(['less'], null, {section:'adm'}) )
 gulp.task('less:index', initRunTasks(['less'], null, {section:'index'}) )
 
-gulp.task('build', initRunTasks(['dist', ['clean']]))
+gulp.task('build', initRunTasks(['dist'], ['clean']))
 
 gulp.task('default', initRunTasks(['watch'], ['nodemon','less'], {section:'all'}) )
 gulp.task('index', initRunTasks(['watch'], ['nodemon','less'], {section:'index'}) )
