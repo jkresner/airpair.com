37c08b6712cd6d9f5c7ee0f9bc36b07337c53d61
Jonathon Kresner <jkresner@gmail.com>
2015-07-18 02:50:26 -0700

    Reduce site anonymous surface
    - Reduce site header links
    - Remove about page
    - No post bookmarks
    - Don’t link to workshops
    - Fix AirPair blog
    - Hide menu for anonymous users

--- ang/posts/module.js
+++ ang/posts/module.js
@@ -12,10 +12,20 @@ angular.module("APPosts", ['APShare', 'APTagInput'])
   route('/posts/fork/:id', 'PostFork', require('./fork.html'), { resolve: authd })
   route('/posts/info/:id', 'PostInfo', require('./info.html'), { resolve: authd })
   route('/posts/edit/:id', 'PostEdit', require('./edit.html'), { resolve: authd })
-  route('/posts/tag/:tagslug', 'PostsTagList', require('./listTag.html'))
   route('/posts/publish/:id', 'PostPublish', require('./publish.html'), { resolve: authd })
   route('/posts/contributors/:id', 'PostContributors', require('./contributors.html'), { resolve: authd })
 
+  route('/posts/tag/:tagslug', 'PostsTagList', require('./listTag.html'))
+  route('/reactjs', 'PostsTagList', require('./listTag.html'))
+  route('/python', 'PostsTagList', require('./listTag.html'))
+  route('/node.js', 'PostsTagList', require('./listTag.html'))
+  route('/ember.js', 'PostsTagList', require('./listTag.html'))
+  route('/keen-io', 'PostsTagList', require('./listTag.html'))
+  route('/rethinkdb', 'PostsTagList', require('./listTag.html'))
+  route('/ionic-framework', 'PostsTagList', require('./listTag.html'))
+  route('/swift', 'PostsTagList', require('./listTag.html'))
+  route('/android', 'PostsTagList', require('./listTag.html'))
+  route('/ruby', 'PostsTagList', require('./listTag.html'))
 })
 
 
@@ -140,9 +150,14 @@ angular.module("APPosts", ['APShare', 'APTagInput'])
 
 })
 
-.controller('PostsTagListCtrl', function($scope, $routeParams, DataService) {
+.controller('PostsTagListCtrl', function($scope, $routeParams, $location, DataService) {
+  console.log('PostsTagListCtrl')
 
-  $scope.tagslug = $routeParams.tagslug;
+  if ($routeParams.tagslug)
+    $scope.tagslug = $routeParams.tagslug
+  else
+    $scope.tagslug = $location.url().replace('/','')
+
   DataService.posts.getTagsPosts({tagSlug:$scope.tagslug}, function (result) {
     $scope.tag = result.tag;
     $scope.tagposts = result.posts;
