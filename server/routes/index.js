98cba4a2d9bbff7f42ee66642c5ef41613a4857a
Jonathon Kresner <jkresner@gmail.com>
2015-08-19 09:13:22 -0700

    Play with anon architecture

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,36 @@
 module.exports = function(routes)
 {
   if (routes == 'whiteList')
-    return require('../../shared/routes')
+    return [
+      '/v1',
+      '/v1/auth*',
+      '/bookings',
+      '/booking/*',
+      '/bookings/*',
+      '/dashboard',
+      '/office',
+      '/posts*',
+      '*/so-welcome',
+      '/settings',
+      '/find-an-expert',
+      '/be-an-expert',
+      '/experts',
+      '/expert-applications',
+      // '/learn',
+      '*/posts*',
+      // '*/workshops*',
+      '/billing*',
+      '/payouts*',
+      '*pair-programming*',
+      '/login',
+      '/me*',
+      '/help*',
+      '/adm*',
+      '/matchmaking*',
+      '/review*',
+      '/requests*',
+      '/100k-writing-competition'
+    ]
   else
     return require(`./${routes}`)
 }
