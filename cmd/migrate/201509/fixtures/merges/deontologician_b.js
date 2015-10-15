c0d3c958b41c170e5347946d6635c209acb349d9
Jonathon Kresner <jkresner@gmail.com>
2015-10-15 12:14:40 +1100

    Add migration app

---
+++ cmd/migrate/201509/fixtures/merges/deontologician_b.js
@@ -0,0 +1,139 @@
+var user = {
+  "_id" : ObjectId("51a9091366a6f999a465f329"),
+  "linked" : {
+      "so" : {
+          "user_id" : 180718,
+          "user_type" : "registered",
+          "creation_date" : 1254170997,
+          "display_name" : "deontologician",
+          "profile_image" : "http://www.gravatar.com/avatar/a9c408ec73a9d18e09c2b94a614da26d?d=identicon&r=PG",
+          "reputation" : 989,
+          "reputation_change_day" : 0,
+          "reputation_change_week" : 10,
+          "reputation_change_month" : 15,
+          "reputation_change_quarter" : 50,
+          "reputation_change_year" : 115,
+          "age" : 28,
+          "last_access_date" : 1370032499,
+          "last_modified_date" : 1370032564,
+          "link" : "http://stackoverflow.com/users/180718/deontologician",
+          "website_url" : "",
+          "location" : "Florida",
+          "account_id" : 60520,
+          "badge_counts" : {
+              "gold" : 0,
+              "silver" : 4,
+              "bronze" : 14
+          },
+          "accept_rate" : 86,
+          "id" : 180718,
+          "token" : {
+              "token" : "9h*bCR5MIS67pl7Ybwz9Aw))",
+              "attributes" : {
+                  "refreshToken" : null
+              }
+          }
+      },
+      "gp" : {
+          "id" : "105549796239265918337",
+          "email" : "deontologician@gmail.com",
+          "verified_email" : true,
+          "name" : "Josh Kuhn",
+          "given_name" : "Josh",
+          "family_name" : "Kuhn",
+          "link" : "https://plus.google.com/105549796239265918337",
+          "picture" : "https://lh6.googleusercontent.com/-8N5Wmt2CP_4/AAAAAAAAAAI/AAAAAAAAAA8/OMF7yWTkAxQ/photo.jpg",
+          "gender" : "male",
+          "locale" : "en",
+          "token" : {
+              "token" : "ya29.1.AADtN_WFzCemG-sP_3t8M9um5phpMmeZ2dhDTzuBeMvNKLdwGMa55IDFYMRH8gNkJOf2jL_l",
+              "attributes" : {}
+          }
+      },
+      "gh" : {
+          "login" : "deontologician",
+          "id" : 1366,
+          "avatar_url" : "https://secure.gravatar.com/avatar/f357fd57f93dd47462fcfc07abe47f7a?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+          "gravatar_id" : "f357fd57f93dd47462fcfc07abe47f7a",
+          "name" : "Josh Kuhn",
+          "company" : null,
+          "blog" : "http://deontologician.tumblr.com",
+          "location" : "St. Petersburg, FL",
+          "email" : "deontologician@gmail.com",
+          "hireable" : false,
+          "bio" : "I'm a curious programmer with a Master's in computer science. My Master's thesis\r\nwas an implementation of a packet classification algorithm in C. I love\r\nprogramming language theory, but I also love the practice.  More generally, I\r\nlove finding new interesting projects.\r\n\r\n## Languages I'm most comfortable with\r\n* C\r\n* Python\r\n* Haskell\r\n\r\n## Languages I use occasionally\r\n* Java\r\n* Standard ML\r\n* Bash Shell script\r\n* Erlang\r\n* C++\r\n\r\n## Languages I've learned in the past, but am rusty with\r\n* Javascript (jQuery)\r\n* C#\r\n* Ruby\r\n* PHP\r\n* Common Lisp\r\n\r\n## Stuff I'm familiar with but don't really consider a programming language\r\n* HTML\r\n* CSS3\r\n* MySQL/PostgreSQL\r\n* JSON\r\n* XHTML (wow, I really backed the wrong horse on this one...)\r\n\r\n## Operating Systems and Tools\r\n\r\nI'm definitely a Linux programmer. That entails being familiar mostly with GNU\r\ntools, but I can find my way around on other *NIX systems. My main editor is\r\nemacs, and I've used Netbeans and Eclipse (and, yes, Visual Studio) in the past,\r\nso I'm comfortable with IDEs. Generally though, I'm more likely to just use\r\nemacs and slap some customizations in to make it easier to work with. \r\n\r\nCoding my master's thesis was a fairly extensive project, and in the course of\r\nit I got quite comfortable with several tools to help me manage it:\r\n\r\n* gdb\r\n* valgrind\r\n* FCTX (a C unit testing framework)\r\n* Autotools\r\n* splint",
+          "public_repos" : 13,
+          "followers" : 5,
+          "following" : 3,
+          "created_at" : "2008-02-28T00:59:44Z",
+          "updated_at" : "2013-05-29T02:58:36Z",
+          "public_gists" : 21,
+          "token" : {
+              "token" : "43992e543c6b29dfa193a8f704825d4ad9f5d170",
+              "attributes" : {}
+          }
+      },
+      "in" : {
+          "firstName" : "Josh",
+          "id" : "OqWlxtTivf",
+          "lastName" : "Kuhn",
+          "token" : {
+              "token" : "ae4ed6e7-8320-4c30-a3a4-dc7f5c69ad98",
+              "attributes" : {
+                  "tokenSecret" : "b1f44891-1751-4211-80ca-d25b5b7b5dd2"
+              }
+          }
+      },
+      "tw" : {
+          "id" : 297590129,
+          "name" : "Josh Kuhn",
+          "screen_name" : "deontologician",
+          "location" : "St. Petersburg, FL",
+          "url" : "http://deontologician.tumblr.com",
+          "description" : "I'm a Python/Haskell/C programmer. I like solving problems and reading interesting things.",
+          "protected" : false,
+          "followers_count" : 10,
+          "friends_count" : 33,
+          "listed_count" : 0,
+          "created_at" : "Thu May 12 19:24:34 +0000 2011",
+          "favourites_count" : 2,
+          "utc_offset" : -18000,
+          "time_zone" : "Eastern Time (US & Canada)",
+          "geo_enabled" : false,
+          "verified" : false,
+          "lang" : "en",
+          "profile_image_url" : "http://a0.twimg.com/profile_images/1370398844/deontologician_normal.png",
+          "profile_banner_url" : "https://pbs.twimg.com/profile_banners/297590129/1364171773",
+          "following" : false,
+          "notifications" : false,
+          "token" : {
+              "token" : "297590129-W9nvKTDZV5X0hm8ON19zaYej1YqvXpP6yy8N1P5w",
+              "attributes" : {
+                  "tokenSecret" : "Xs26j24jCXOQC6q2A6Os5I5eQP0ilNeXIX6QqLPWk"
+              }
+          }
+      }
+  },
+  "name" : "Josh Kuhn",
+  "email" : "deontologician@gmail.com"
+}
+
+var expert = null
+
+
+var suggests = [
+
+]
+
+
+var booked = [
+
+]
+
+
+var ordered = [
+
+]
+
+
+module.exports = { user, expert, suggests, booked }
---
+++ cmd/migrate/run.coffee
@@ -0,0 +1,39 @@
+# cmd/npm/run-db-migrate
+
+colors                       = require('colors')
+global.$log                  = console.log
+global._                     = require('lodash')
+global.moment                = require('moment')
+global.domain                = require('domain')
+global.fs                    = require('fs')
+global.util                  = require('meanair-shared.util')
+global.idToDate              = util.MongoUtil.idToDate
+global.idToMoment            = util.MongoUtil.idToMoment
+
+
+
+global.specInit = (ctx) ->
+  ctx.timeout(4000000)
+  before ->
+    global.Tags = DB.Collections.tags
+    global.Users = DB.Collections.users
+    global.Experts = DB.Collections.experts
+    global.Bookings = DB.Collections.bookings
+    global.Requests = DB.Collections.requests
+    global.Orders = DB.Collections.orders
+    global.Posts = DB.Collections.posts
+    global.Paymethods = DB.Collections.paymethods
+    global.Payouts = DB.Collections.payouts
+    global.ORDER = FIXTURE.orders
+    global.EXPERT = FIXTURE.experts
+
+
+
+
+SCREAM          = require('meanair-scream')
+
+
+config          = require('./201509/setup')('test')
+
+
+SCREAM(__dirname, config).run()
