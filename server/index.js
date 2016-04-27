856fe3aaa42a355601b86f3bc862cbebb334ecc9
Jonathon Kresner <jkresner@gmail.com>
2016-04-27 10:58:52 +1000

    ugly hacking

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-var MAServer  = require('meanair-server')
-var config    = MAServer.Config(__dirname, process.env.ENV || 'dev', true)
-var tracking  = require('./app.track')
+// process.env.ENV = 'prod'
+var MAServer    = require('meanair-server')
+var config      = MAServer.Config(__dirname, process.env.ENV || 'dev', true)
+var tracking    = require('./app.track')
+
+console.log('config', config)
+
+config.routes.redirects.on = true
 
 require('./app').run({ config, MAServer, tracking },
     e => e ? $log('APP.ERROR'.red, e) : '')
