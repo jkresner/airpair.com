b3a046f63a199f96d55951ab92853b1168f3383e
Jonathon Kresner <jkresner@gmail.com>
2016-05-22 08:48:21 -0700

    font-end

--- ang/adm/redirects/module.js
+++ ang/adm/redirects/module.js
@@ -1,34 +1,33 @@
-
 angular.module("ADMRedirects", [])
 
-  .config(function($locationProvider, $routeProvider) {
+.config(function($locationProvider, $routeProvider) {
 
-    $routeProvider.when('/adm/redirects', {
-      template: require('./list.html'),
-      controller: 'RedirectsCtrl as redirects'
-    });
-
+  $routeProvider.when('/adm/redirects', {
+    template: require('./list.html'),
+    controller: 'RedirectsCtrl as redirects'
   })
 
-  .controller('RedirectsCtrl', function($scope, AdmDataService) {
+})
 
-    AdmDataService.getRedirects(function (result) {
-      $scope.redirects = result;
+.controller('RedirectsCtrl', function($scope, AdmDataService) {
+
+  AdmDataService.getRedirects(function (result) {
+    $scope.redirects = result;
+  })
+
+  $scope.createRedirect = function() {
+    var d = { previous: $scope.previous, current: $scope.current, type: $scope.type }
+    AdmDataService.createRedirect(d, function (result) {
+
+      $scope.redirects = _.union([result],$scope.redirects)
     })
+  }
 
-    $scope.createRedirect = function() {
-      var d = { previous: $scope.previous, current: $scope.current, type: $scope.type }
-      AdmDataService.createRedirect(d, function (result) {
+  $scope.deleteRedirect = function(id) {
+    AdmDataService.deleteRedirect(id, function () {
+      var r = _.find($scope.redirects, (r) => r._id == id )
+      $scope.redirects = _.without($scope.redirects, r)
+    })
+  }
 
-        $scope.redirects = _.union([result],$scope.redirects)
-      })
-    }
-
-    $scope.deleteRedirect = function(id) {
-      AdmDataService.deleteRedirect(id, function () {
-        var r = _.find($scope.redirects, (r) => r._id == id )
-        $scope.redirects = _.without($scope.redirects, r)
-      })
-    }
-
-  })
+})
