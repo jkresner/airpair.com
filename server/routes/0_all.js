084f553eb0ec790b2a1870065712316de9ae82bb
Jonathon Kresner <jkresner@gmail.com>
2016-05-29 15:37:35 +1000

    Tweaks

--- server/routes/0_all.js
+++ server/routes/0_all.js
@@ -3,9 +3,9 @@ module.exports = function(app, mw, {}) {
   if (mw.$.abuser)
     app.use(mw.$.abuser)
 
-
-  if (config.middleware.slow)
-    app.use(mw.req.slow(config.middlware.slow, {
+  var {slow} = config.middleware
+  if (slow)
+    app.use(mw.req.slow(slow, {
       onSlow({ctx,originalUrl,method,body}, duration) {
         analytics.issue(ctx, 'req:slow', 'performance',
           assign({ duration, url: originalUrl, method }, body ? {body} : {}) )
